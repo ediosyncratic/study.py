3e9a54848a84eb4f1e18f11fa9d311a652c6f728
Edward Welbourne <eddy@chaos.org.uk>
1999-05-09 14:35:43 +0000

    Initial revision

---
+++ value/SI.py
@@ -0,0 +1,129 @@
+"""The SI units.
+
+$Id: SI.py,v 1.1 1999-05-09 14:35:43 eddy Exp $
+"""
+from quantity import *
+
+# The base units
+s = second = base_unit('s', 'second',
+		      """The SI base unit of time.
+
+9192631770 periods of the radiation corresponding to the transition between
+the two hyperfine levels of the ground state of  the caesium-133 atom.""")
+
+m = metre = base_unit('m', 'metre',
+		     """The SI base unit of length.
+
+The length of the path travelled by light in vacuum during a time interval of
+1/299792458 of a second. """)
+
+# speed of light is 299792458 * metre / second
+
+kg = kilogram = kilogramme = base_unit('kg', 'kilogramme',
+                                        """The SI base unit of mass.
+
+The mass of the *International Prototype* kilogramme (a platinum-iridium
+cylinder) kept in the Bureau International des Poids et Mesures (BIPM),
+S&egrave;vres, Paris.""")
+
+A = Ampere = base_unit('A', 'Ampere',
+		      """The SI base unit of electric current.
+
+That constant current which, if maintained in two parallel rectilinear
+conductors, of immense length and negligible circular cross-section, placed 1
+metre apart in a vacuum, would produce a force between these conductors equal to
+2e-7 newton per meter of length.""")
+
+K = Kelvin = base_unit('K', 'Kelvin',
+		      """The SI base unit of temperature.
+
+The fraction 1/273.16 (exactly) of the thermodynamic temperature at the triple
+point of water. """)
+
+mol = base_unit('mol', 'Mole',
+	       """The SI base unit of `amount of substance'.
+
+The amount of substance which contains as many elementary units as there are
+atoms in 12e-3 kilogrammes (exactly) of pure carbon-12.  The elementary unit
+must be specified and may be atom, molecule, ion, radical, electron, photon
+<I>etc</I>. or collection of elementary units. """)
+
+cd = candela = base_unit('cd', 'Candela',
+			"""The SI base unit of luminous intensity.
+
+The luminous intensity, in a given direction, of a source that emits
+monochromatic radiation of frequency 540e12 Hz and that has a radiant intensity
+in that direction of 1/683 W/sr.
+
+The luminous intensity, in the perpendicular direction, of a surface of 1/60
+square centimetere of a black body at the freezing temperature of platinum under
+a pressure of 101325 Pascal (1 atmosphere).""")
+
+rad = radian = base_unit('rad', 'Radian',
+			"""The SI supplementary unit of angle.
+
+The angle subtended at the centre of a circle by an arc of the circumference
+equal in length to the radius of the circle.""")
+
+sr = steradian = base_unit('sr', 'Steradian',
+			  """The SI supplementary unit of solid angle.
+
+The unit of solid angle is the solid angle subtended at the center of a sphere
+of radius r by a portion of the surface of the sphere having area r*r.""")
+
+# Composite SI units
+N = Newton = kilogramme * metre / second / second	# Force
+Hz = Hertz = 1 / second		# Frequency
+C = Coulomb = Ampere * second	# Charge
+J = Joule = Newton * metre	# Energy
+W = Watt = Joule / second	# Power
+Pa = Pascal = Newton / metre / metre	# Pressure
+lm = lumen = candela * steradian	# Luminous flux
+lx = lux = lumen / metre / metre	# Illumination
+stere = pow(metre, 3)
+litre = milli * stere
+
+V = Volt = Joule / Coulomb	# Electromagnetic potential
+Wb = Weber = Joule / Ampere	# Magnetic flux
+Ohm = Volt / Ampere		# Electrical resistance
+S = Siemens = 1 / Ohm		# Conductance
+F = Farad = second / Ohm	# Capacitance
+H = Henry = Weber / Ampere	# Inductance
+T = Tesla = Weber / metre / metre	# Magnetic flux density
+
+# Other units
+minute = 60 * second
+hour = 60 * minute
+day = 24 * hour
+week = 7 * day
+fortnight = 2 * week
+year = (365 + 1/ (4 + 1/(25 + 1/4.))) * day	# Gregorian
+# and my working year has 8 bank holidays + 25 my holidays.
+
+gram = milli * kilogramme
+cm = centi * metre
+cc = pow(cm, 3)
+
+Angstrom = .1 * nano * metre    # &Aring;ngstr&ouml;m, aka &Aring;.
+micron = micro * metre
+fermi = femto * metre
+are = pow(metre, 2)
+hectare = hecto * are
+barn = femto * hectare
+
+St = Stokes = pow(cm, 2) / second       # kinematic viscosity
+Gs = Gauss = .1 * milli * Tesla
+gamma = nano * Tesla
+Mx = Maxwell = 10 * nano * Weber
+stilb = 10 * kilo * candela / metre / metre
+phot = 10 * kilo * lux
+
+Bq = Becquerel = Hz             # Activity of a radionuclide
+Gy = Gray = Joule / kilogramme  # Absorbed dose of radiation
+Sv = sievert = Gy               # Dose equivalent
+rem = 10 * milli * Sv
+# 10 milli Gray is also called a rad (conflicts with radian)
+Ci = Curie = 37 * giga * Becquerel
+R = Rontgen = .258 * milli * Coulomb / kilogramme       # R&ouml;ntgen
+P = poise = Pascal * second     # dynamic viscosity
+
