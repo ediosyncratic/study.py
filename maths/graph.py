1f633abecb4cb3da90a1495b60c5305e9db13df5
Edward Welbourne <eddy@chaos.org.uk>
2000-02-19 13:37:30 +0000

    Moved all node/index translations to Graph so FindUnite's node values are all indices into lists.  Now to carve up FindUnite.

--- maths/graph.py
+++ maths/graph.py
@@ -1,10 +1,13 @@
 """Some graph-theoretic stuff.
 
-$Id: graph.py,v 1.1 2000-02-19 12:18:45 eddy Exp $
+$Id: graph.py,v 1.2 2000-02-19 13:37:30 eddy Exp $
 """
 
 class NodeError(KeyError):
     """Exception used by graph.py's tools to signal `no such node'."""
+
+class Partition:
+    def __init__(self): pass
 
 class FindUnite:
     """An implementation of the find-unite algorithm.
@@ -17,29 +20,15 @@ class FindUnite:
     def __init__(self):
         """Initialises an empty FindUnite."""
 
-        self.__indices = {}     # { arbitrary node: index }
-
-        # Indices are into the following three sequences, each of which is a
-        # lazily-grown infinite list [ 0, 1, 2, 3, ... ] and represents a
-        # network of pointers among the legitimate indices: chase __up to a node
-        # which is its own __up to find the identifying member of your
-        # equivalence class; chasing forward runs around a loop of all nodes in
-        # your equivalence class; chasing backward traverses the same loop in
-        # reverse order.
-
         self.__up = []
         self.__forward = []
         self.__backward = []
 
         # Book-keeping assistant: same length as all the above
         self.__count = []       # number of nodes `below'
-        self.__nodes = []       # inverse of __indices
 
-    def __len__(self): return len(self.__nodes)
-    # assert: always equal to len()s of the other lists and, indeed, of __indices
-
-    def nodes(self): return self.__nodes[:] # a copy !
-    # assert: always equal to self.__indices.keys() modulo order
+    def __len__(self): return len(self.__up)
+    # assert: always equal to len()s of the other lists
 
     def peers(self, node):
         """Returns the equivalence class of a given node.
@@ -51,16 +40,11 @@ class FindUnite:
         If node is unknown to self, self knows of no nodes connected to it; so
         the result is just [ node ]. """
 
-        try: first = self.__indices[node]
-        except KeyError: return [ node ]
-
-        f, names = self.__forward, self.__nodes
-        row, n = [ node ], f[first]
-        while n is not first:
-            row.append(self.__nodes[n])
+        f, n, row = self.__forward, node, []
+        while 1:
+            row.append(n)
             n = f[n]
-
-        return row
+            if n is node: return row
 
     def __chase(self, node):
         """Chases the __up chain from a node to its top.
@@ -69,69 +53,42 @@ class FindUnite:
         chasing i->self.__up[i] to a fixed point.  In the process, it ensures
         the chase will be shorter, in future, for all the nodes chased. """
 
-        try: bot = self.__indices[node]
-        except KeyError: raise NodeError(node)
-
-        ans = bot
+        trail = []
         while 1:
-            a = self.__up[ans]
-            if a is ans: break
-            ans = a
+            n = self.__up[node]
+            if n is node: break
+            trail.append(node)
+            node = n
 
-        # short-cut future chases
-        while bot is not ans:
-            next, self.__up[bot] = self.__up[bot], ans
-            bot = next
-
-        return ans
+        # All but the last item in trail need to be pointed at node:
+        for n in trail[:-1]: self.__up[n] = node
+        return node
 
     def joined(self, node, *nodes):
         """Do the edges seen thus far connect (all) the given nodes ?
 
-        Arguments are nodes; at least one must be given.  If any of the nodes
-        given is unknown to self, even if it's the only one given, the result is
-        false: self doesn't join it to anything.  Otherwise, result is true
+        Arguments are nodes; at least one must be given.  Result is true
         precisely if all nodes are in the same connected component of the graph
         of edges thus far passed to self.join(). """
 
-        try:
-            nod = self.__chase(node)
-            for vertex in nodes:
-                if nod != self.__chase(vertex):
-                    return None # i.e. No.
-
-        except NodeError: # Unknown node
-            return None
+        nod = self.__chase(node)
+        for vertex in nodes:
+            if nod != self.__chase(vertex):
+                return None # i.e. No.
 
         return 1>0 # i.e. Yes
 
-    def __newnode(self, node):
-        # Create a new node:
-        self.__indices[node] = ind = len(self.__nodes)
-        self.__nodes.append(node)
+    def append(self, ind):
+        if ind != len(self.__up):
+            raise ValueError, "FindUnite's append requires the right value"
+
         assert len(self.__forward) == ind == len(self.__backward)
         # Lazily grow the lists: node points at itself initially in all.
         self.__forward.append(ind)
         self.__backward.append(ind)
         self.__up.append(ind)
-        # New node has only itself `below'.
+        # New node's sub-tree contains only itself:
         self.__count.append(1)
-
-        # Return the index we just gave it.
-        return ind
-
-    def __root(self, node):
-        """Either creates node or finds its parent.
-
-        If node is new to self, it is allocated a number and internal
-        data-structures are updated to accommodate it.  Thereafter, this number
-        is self's index for that node.  Otherwise, node already exists and we
-        chase i-> self.__up[i] to find the representative of its connected
-        component. """
-
-        try: return self.__chase(node)
-        except NodeError:
-            return self.__newnode(node)
 
     def join(self, node, vertex):
         """Joins a given pair of nodes.
@@ -141,37 +98,76 @@ class FindUnite:
         known to self are added, initially connected to nothing; they are then
         joined as for familiar nodes. """
 
-        nod, ver = self.__root(node), self.__root(vertex)
+        nod, ver = self.__chase(node), self.__chase(vertex)
         if nod is ver: return # nothing to do
 
-        # Hang one node below the other:
-        count = self.__count
+        # Who has the bigger sub-tree ?
+        count = self.__count # we refer to it 5 times, may as well use local ref
         if count[nod] < count[ver]: nod, ver = ver, nod # swap
 
+        # Hang smaller tree below bigger:
         self.__up[ver] = nod
         count[nod] = count[nod] + count[ver]
         del count # we're done with this reference to it
 
         # Now combine their two-way loops:
         f, b = self.__forward, self.__backward
-        p, w = f[nod], f[ver]
-        assert nod is b[p] and ver is b[w]
-        # Do the swap:
-        b[w], b[p] = nod, ver
-        f[nod], f[ver] = w, p
-        return
+        pod, wer = f[nod], f[ver]
+        assert b[pod] is nod and b[wer] is ver
+        # Do the swaps:
+        b[wer], b[pod] = nod, ver
+        f[nod], f[ver] = wer, pod
 
 class Graph:
     def __init__(self):
-        self.__edges, self.__equiv = [], FindUnite()
+        self.__edges, self.__nodes, self.__equiv = [], [], FindUnite()
+
+    # Read-only copies:
+    def nodes(self): return tuple(self.__nodes)
+    def edges(self): return tuple(self.__edges)
+
+    def __node(self, node):
+        """Returns internal index of node.
+
+        If node has been met before, an index has been recorded for it: this is
+        returned.  Otherwise, the node is added to internal datastructures with
+        a previously-unused index, which is returned.  For internal use only. """
+
+        # Could be more efficient using an inverse-lookup here.
+        try: return self.__nodes.index(node)
+        except ValueError: pass
+
+        ind = len(self.__nodes)
+        self.__nodes.append(node)
+
+        self.__equiv.append(ind)
+        return ind
 
     def newedge(self, start, stop, twoway=1>0):
-        self.__equiv.join(start, stop)
         self.__edges.append((start, stop))
+        self.__equiv.join(self.__node(start), self.__node(stop))
+
+    def peers(self, node):
+        try: nod = self.__nodes.index(node)
+        except ValueError: return [ node ]
+
+        return map(lambda i, _r=self.__nodes: _r[i],
+                   self.__equiv.peers(nod))
+
+    def joined(self, *nodes):
+        try: indices = map(self.__nodes.index, nodes)
+        except ValueError: return None
+
+        return apply(self.__equiv.joined, indices)
+        
 
 """
  $Log: graph.py,v $
- Revision 1.1  2000-02-19 12:18:45  eddy
+ Revision 1.2  2000-02-19 13:37:30  eddy
+ Moved all node/index translations to Graph so FindUnite's node values
+ are all indices into lists.  Now to carve up FindUnite.
+
+ Revision 1.1  2000/02/19 12:18:45  eddy
  Initial revision
 
 """
