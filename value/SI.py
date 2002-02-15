3a4220d7145689a65bdc01e4c330a0235e904d6c
Edward Welbourne <eddy@chaos.org.uk>
2002-02-15 16:15:20 +0000

    Moved the Mass/Time bodges here (from units.py). Added units of textile fineness, tex and dtex.

--- value/SI.py
+++ value/SI.py
@@ -1,15 +1,36 @@
 """The SI units.
 
-$Id: SI.py,v 1.3 2002-02-11 01:32:49 eddy Exp $
+See also units.py for a huge bestiary of other units.
+
+$Id: SI.py,v 1.4 2002-02-15 16:15:20 eddy Exp $
 """
 from quantity import *
 
+# there has to be a better way to do these ...
+
+# tweak masses to support .force:
+class Mass (Quantity):
+    def _lazy_get_force_(self, ignored):
+	"""weight or force of a mass under `standard gravity'"""
+	return self * 9.80665 * metre / second**2
+    _lazy_get_weight_ = _lazy_get_force_
+
+    _quantity = Quantity
+
+# and times to support .light:
+class Time (Quantity):
+    def _lazy_get_light_(self, ignored):
+	"""distance travelled by light in a given amount of time"""
+	return self * 299792458 * metre / second
+
+    _quantity = Quantity
+
 # The base units [kg and s get more sophisticated in units.py]
-second = base_unit('s', 'second',
-                      """The SI base unit of time.
+second = sec = s = Time(base_unit('s', 'second',
+				  """The SI base unit of time.
 
 9192631770 periods of the radiation corresponding to the transition between
-the two hyperfine levels of the ground state of  the caesium-133 atom.""")
+the two hyperfine levels of the ground state of  the caesium-133 atom."""))
 
 m = metre = base_unit('m', 'metre',
                      """The SI base unit of length.
@@ -17,18 +38,12 @@ m = metre = base_unit('m', 'metre',
 The length of the path travelled by light in vacuum during a time interval of
 1/299792458 of a second. """)
 
-second.also(light = Quantity(299792458, metre,
-                             doc = """The distance light travels each second in a vacuum.
-
-This is exact, being now the definition of the metre."""))
-# see also units.Time, which supplies the same datum systematically for times ...
-
-kilogramme = base_unit('kg', 'kilogramme',
-                                        """The SI base unit of mass.
+kilogramme = kilogram = kg = Mass(base_unit('kg', 'kilogramme',
+					    """The SI base unit of mass.
 
 The mass of the *International Prototype* kilogramme (a platinum-iridium
 cylinder) kept in the Bureau International des Poids et Mesures (BIPM),
-S&egrave;vres, Paris.""")
+S&egrave;vres, Paris."""))
 
 A = Ampere = base_unit('A', 'Ampere',
                       """The SI base unit of electric current.
@@ -97,9 +112,9 @@ T = Tesla = Weber / metre / metre       # Magnetic flux density
 
 P = poise = Pascal * second       # dynamic viscosity
 
-# Other units
-gram = milli * kilogramme
-cm = centi * metre
+# Other SI-compatible units
+gram = Mass(milli * kilogramme)
+km, cm = kilo * metre, centi * metre
 cc = pow(cm, 3)
 
 St = Stokes = pow(cm, 2) / second # kinematic viscosity
@@ -121,10 +136,17 @@ Gy = Gray = Joule / kilogramme  # Absorbed dose of radiation
 Sv = sievert = Gy               # Dose equivalent
 rem = 10 * milli * Sv
 # 10 milli Gray is also called a rad (conflicts with radian)
+
+tex = gram / km # fineness of textiles
+dtex = deci * tex # see also: units.denier
 
 """
  $Log: SI.py,v $
- Revision 1.3  2002-02-11 01:32:49  eddy
+ Revision 1.4  2002-02-15 16:15:20  eddy
+ Moved the Mass/Time bodges here (from units.py).
+ Added units of textile fineness, tex and dtex.
+
+ Revision 1.3  2002/02/11 01:32:49  eddy
  Migrated some aliasing to units.py, moved light-speed to a datum of times,
  repositioned poise and Stokes.  Untabified.
 
