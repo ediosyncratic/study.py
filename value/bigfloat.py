c00d3061d05f8b96a13b8f86e5705f66cfcc4616
Edward Welbourne <eddy@chaos.org.uk>
2003-09-22 22:15:48 +0000

    Fixed bug in add (forgot factors of 100 on centuries !), made lazy; implemented float, int, long, divmod, exp, log, hash.

--- value/bigfloat.py
+++ value/bigfloat.py
@@ -1,11 +1,17 @@
 """Representing *very* big floating-point numbers.
+
+ToDo:
+  * make it work with Quantity, i.e. with Sample, i.e. with Weighted
+  * make it work with complex
 """
 
 _rcs_id_ = """
-$Id: bigfloat.py,v 1.3 2003-09-22 07:20:32 eddy Exp $
+$Id: bigfloat.py,v 1.4 2003-09-22 22:15:48 eddy Exp $
 """
+
+from basEddy.lazy import Lazy
 
-class BigFloat:
+class BigFloat (Lazy):
 
     def __init__(self, val=1, century=0):
         if century != long(century): raise ValueError, 'century must be whole'
@@ -16,32 +22,37 @@ class BigFloat:
             val, century = val.__scale, century + val.__century
 
         if val:
+            while abs(val) > BigFloat.huge: val, century = val / BigFloat.step, century + 1L
             while abs(val) > 1e50: val, century = val * 1e-100, century + 1L
             while abs(val) < 1e-50: val, century = val * 1e100, century - 1L
         else: century = 0
 
         self.__century, self.__scale = long(century), float(val)
 
-    def __str__(self):
+    def __str__(self): return self.str
+    __repr__ = __str__
+    # def __repr__(self): return 'BigFloat(%g, %dL)' % (self.__scale, self.__century)
+
+    def _lazy_get_str_(self, ignored):
         ans = '%e' % self.__scale
         siz, dec = ans.split('e') # raise exception unless exactly one 'e'
         dec = int(dec) + 100 * self.__century
         return '%se%d' % (siz, dec)
 
-    __repr__ = __str__
-    # def __repr__(self): return 'BigFloat(%g, %dL)' % (self.__scale, self.__century)
-
     def __nonzero__(self): return self.__scale != 0
+    def _lazy_get__lazy_hash_(self, ignored): return hash(self.__scale) ^ hash(self.__century)
     def __neg__(self): return BigFloat(-self.__scale, self.__century)
     def __abs__(self): return BigFloat(abs(self.__scale), self.__century)
-    def __long__(self): return long(float(self))
     def __int__(self): return int(float(self))
+    def __long__(self): return self.long
+    def __float__(self): return self.float
 
-    def __float__(self):
+    def _lazy_get_float_(self, ignored):
         val, cen = self.__scale, self.__century
+
         while cen > 0:
             val, cen = val * 1e100, cen - 1
-            if val == val / 10: return val
+            if val == val / 10: return val # infinite
 
         while cen < 0:
             val, cen = val * 1e-100, cen + 1
@@ -49,9 +60,30 @@ class BigFloat:
 
         return val
 
+    def __divmod__(self, other):
+        # Need to solve: self = quot * other + rem;
+        # quot must be whole, rem must be between 0 (included) and other (excluded).
+        if 0 <= self < other or 0 >= self > other: return 0L, self
+
+        # determine order of magnitude of quotient:
+        q = 10L ** long(str(self / other).split('e')[1])
+
+        # get on with refining a value ...
+        quot, rem = 0L, self
+        while q:
+            d = int(rem / other / q)
+            if d: quot, rem = quot + d * q, rem - d * q * other
+            assert int(rem / other / q) == 0
+            q = q / 10
+        
+        return quot, rem
+
     def extract(other): # local function, not method
         try: return other.__scale, other.__century
-        except AttributeError: return other, 0L
+        except AttributeError: pass
+        try: other = BigFloat(other)
+        except ValueError: return other, 0L # leave problem for caller !
+        return other.__scale, other.__century
 
     def __mul__(self, other, get=extract):
         val, cen = get(other)
@@ -89,15 +121,15 @@ class BigFloat:
         if self.__scale == 0: return BigFloat(other)
         val, cen = get(other)
         if val == 0: return self
-
-        way = cmp(cen, self.__century)
-        if way == 0: return BigFloat(self.__scale + val, cen)
+        if cen == self.__century: return BigFloat(self.__scale + val, cen)
 
         # ah !  messier ... one is huge compared to the other.
-        this, that = self.__century + log(self.__scale), cen  + log(val)
-        # is one of the numbers ignorable compared to the other ?
-        if this < BigFloat.logeps + that: return BigFloat(other)
-        if this + BigFloat.logeps > that: return self
+        this, that = self.__century * 100 + log(self.__scale), cen * 100  + log(val)
+        # Is one number ignorable compared to the other ?
+        if this + 1 < BigFloat.logeps + that: return BigFloat(other)
+        if this + BigFloat.logeps > that + 1: return self
+
+        # No, we really have to do this the hard way ...
         mid = (this + that) / 2
         val = self.__scale * 10.**(this - mid) + val * 10.**(that - mid)
         # answer is now val * 10.**mid
@@ -109,6 +141,17 @@ class BigFloat:
         return BigFloat(val * 10.**mid, cen)
 
     del extract
+
+    def _lazy_get_long_(self, log=decade):
+        val, cen = self.__scale, self.__century
+        if cen < 0: return 0L
+        if cen == 0:
+            if abs(val) < 1: return 0L
+            return long(val)
+
+        dec = log(val) # >= -50
+        gap = BigFloat.loginf + BigFloat.logeps - 50 - dec # < loginf + logeps
+        return long(val * 10. ** gap) * 10L**(100L * cen - gap)
 
     def __pow__(self, other, log=decade):
         whole, frac = divmod(other, 1)
@@ -145,6 +188,20 @@ class BigFloat:
     def __sub__(self, other): return self + (-other)
     def __rsub__(self, other): return (-self) + other
 
+    import math
+    def _lazy_get_exp_(self, ignored, exp=math.exp):
+        # Obviously, this will get you into trouble if self is big !
+        # exp(self) = expval * 1e100**expcen
+        # self = log(expval) + expcen*log(1e100)
+        c, v = divmod(self, BigFloat.logcentury)
+        return BigFloat(exp(v), c)
+
+    def _lazy_get_log_(self, ignored, log=math.log):
+        return log(self.__scale) + BigFloat.logcentury * BigFloat(self.__century)
+
+    logcentury = 100 * math.log(10)
+    del math
+
     i, x = 0, 1.
     while x > 0: i, x = i - 1, x * .1
     logzero = i
@@ -155,12 +212,19 @@ class BigFloat:
 
     i, x = 0, 1.
     while x + 1 != 1: i, x = i-1, x * .1
-    logeps = i
+    logeps = i # loginf + logzero - 1
+
+    huge, step = 10L ** (loginf + logeps), 10L ** 100
+
     del i, x
 
 _rcs_log_ = """
 $Log: bigfloat.py,v $
-Revision 1.3  2003-09-22 07:20:32  eddy
+Revision 1.4  2003-09-22 22:15:48  eddy
+Fixed bug in add (forgot factors of 100 on centuries !), made lazy;
+implemented float, int, long, divmod, exp, log, hash.
+
+Revision 1.3  2003/09/22 07:20:32  eddy
 Added support for abs, long, int, float
 
 Revision 1.2  2003/09/21 17:29:23  eddy
