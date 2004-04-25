133119791fdea848687a99481a43ec452c2b662f
Edward Welbourne <eddy@chaos.org.uk>
2004-04-25 15:28:08 +0000

    Use exact values in cache, yield exact values from refiners. Also, flush cache after each rummage.

--- maths/root.py
+++ maths/root.py
@@ -1,7 +1,7 @@
 """Solving an equation in one dimension.
 
 See search.Search for 2 dimensions (represented by complex).
-$Id: root.py,v 1.2 2004-04-25 14:06:46 eddy Exp $
+$Id: root.py,v 1.3 2004-04-25 15:28:08 eddy Exp $
 """
 
 class Search:
@@ -9,18 +9,21 @@ class Search:
     """
 
     def __init__(self, func, guess, goal=abs, stride=1):
-        self.goal, self.stride = goal, stride
-
-        # Determine whether to use evaluate or not:
-        try: first = guess.evaluate(func)
-        except AttributeError:
-            first = func(guess)
-            self.__func = func
-        else: self.__func = lambda x, f=func: x.evaluate(f)
+        self.__func, self.goal, self.stride = func, goal, stride
 
         # Initialise cache, best
-        self.__cache = { guess: first }
-        self.__best = (guess, goal(first))
+        self.__cache = {}
+        self.__best = (None, 1e1024)
+        self.func(guess)
+        assert self.__best[0] is not None, "Whacky goal function you've got there ..."
+
+    def __call_func(self, val):
+        try: return self.__cache[val]
+        except KeyError: pass
+        ans = self.__func(val)
+        self.__cache[val], score = ans, self.goal(ans)
+        if score < self.__best[1]: self.__best = (val, score)
+        return ans
 
     def __getattr__(self, key):
         if key == 'best': return self.__best[0]
@@ -29,12 +32,8 @@ class Search:
         raise AttributeError, key
 
     def func(self, val):
-        try: return self.__cache[val]
-        except KeyError: pass
-        ans = self.__func(val)
-        self.__cache[val], score = ans, self.goal(ans)
-        if score < self.__best[1]: self.__best = (val, score)
-        return ans
+        try: return val.evaluate(self.__call_func)
+        except AttributeError: return self.__call_func(val)
 
     def __flush(self, keep=10):
         if len(self.__cache) > keep:
@@ -42,7 +41,7 @@ class Search:
             row.sort()
             for (s,k) in row[keep:]: del self.__cache[k]
 
-    def logrange(scale=1, count=10, step=-.5):
+    def logrange(scale=1, count=10, step=-.5): # local function, not method
         row = []
         while count > 0:
             row.append(scale)
@@ -65,14 +64,22 @@ class Search:
 
     del logrange
 
-    def Raphson(self, val=None, step=None):
+    def exact(val): # local function, not method.
+        try: zero = val.evaluate(lambda x: 0)
+        except AttributeError: zero = val * 0
+        try:
+            while val.width > zero: val = val.best
+        except AttributeError: pass
+        return val
+
+    def Raphson(self, val=None, step=None, hit=exact):
         if val is None: val = self.best
-        move = - self.func(val) / self.gradient(val, step)
+        move = - hit(self.func(val)) / hit(self.gradient(val, step))
         if move: self.stride = move
-        ans = val + move
+        ans = hit(val + move)
         return ans, self.goal(self.func(ans))
 
-    def chord(self, val=None, step=None):
+    def chord(self, val=None, step=None, hit=exact):
         if val is None: val = self.best
         if not step: step = self.stride
         self.func(val), self.func(val + step)
@@ -94,17 +101,19 @@ class Search:
 
         cut.sort()
         ig, (x,y), (k,v) = cut[0]
-        rate = (v-y)/(k-x)
+        rate = hit(v-y)/hit(k-x)
         if rate:
-            move = - y / rate
+            move = - hit(y / rate)
             if move: self.stride = move
-            ans = x + move
-        else: ans = (k + x) / 2
+            ans = hit(x + move)
+        else: ans = hit(k + x) * .5
 
         return ans, self.goal(self.func(ans))
 
+    del exact
+
     def rummage(self, tries=42, threshold=-1):
-        best = self.score
+        good = self.score
         while tries > 0:
             tries = tries - 1
             if tries % 3:
@@ -114,13 +123,22 @@ class Search:
                 try: self.chord()
                 except IndexError: continue
 
-            if best < self.score:
-                if self.score < threshold: return self.__best
-                best = self.score
+            if good < self.score: # improvement
+                if self.score < threshold: # victory
+                    self.__flush()
+                    return self.__best
+
+                good = self.score
+
+        self.__flush(16)
 
 _rcs_log = """
  $Log: root.py,v $
- Revision 1.2  2004-04-25 14:06:46  eddy
+ Revision 1.3  2004-04-25 15:28:08  eddy
+ Use exact values in cache, yield exact values from refiners.
+ Also, flush cache after each rummage.
+
+ Revision 1.2  2004/04/25 14:06:46  eddy
  Fix dumb error in chord, and record its step size when sensible.
 
  Initial Revision 1.1  2004/04/25 13:56:17  eddy
