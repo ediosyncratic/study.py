711e9a57891c0e7e8061c703e5d7495fad3d3d90
Edward Welbourne <eddy@chaos.org.uk>
2005-03-12 12:58:26 +0000

    Initial revision

---
+++ space/__init__.py
@@ -0,0 +1,12 @@
+"""Data about stars, planets and other clutter in space.
+
+$Id: __init__.py,v 1.1 2005-03-12 12:58:26 eddy Exp $
+"""
+
+
+_rcs_log = """
+$Log: __init__.py,v $
+Revision 1.1  2005-03-12 12:58:26  eddy
+Initial revision
+
+"""
