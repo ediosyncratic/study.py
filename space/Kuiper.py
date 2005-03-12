87d5d534365b6ee1ef80b48e229c683b72d1c889
Edward Welbourne <eddy@chaos.org.uk>
2005-03-12 15:21:04 +0000

    Initial revision

---
+++ space/Kuiper.py
@@ -0,0 +1,74 @@
+# -*- coding: iso-8859-1 -*-
+"""Kuiper Belt and Oort Cloud objects of our Solar system.
+
+$Id: Kuiper.py,v 1.1 2005-03-12 15:25:57 eddy Exp $
+"""
+
+from basEddy.units import *
+from space.home import Sun, KLplanet, KLsurface
+from space.common import Orbit, Spin, Discovery, Spheroid
+from space.body import Planet
+
+Pluto = KLplanet('Pluto',
+                 KLsurface(.23, .05, Spin(6 * day + 9 * hour, 118),
+                           flattening = 0, material="CH4 ice"),
+                 Orbit(Sun, (5936 + .1 * tophat) * giga * metre,
+                       Spin(250 * year, 17.13), .253),
+                 .0025, 1.1, atmosphere="trace CH4")
+Pluto.mass.observe(15e21 * kilogramme)
+Pluto.surface.spin.period.observe(6.3867 * day)
+Pluto.surface.radius.observe(mega * (1.195 + .001 * tophat) * metre) # NASA
+Pluto.orbit.spin.period.observe(248.5 * 365.242198781 * day)
+Pluto.orbit.spin.period.observe(248.02 * year) # NASA
+Pluto.orbit.radius.observe(giga * (5915.80 + .1 * tophat) * metre) # NASA
+
+Quaoar = Planet('Quaoar', Spheroid(800 * mile),
+                # I haven't yet found radius ... but its eccentricity is low
+                Orbit(Sun, Quantity(4 + tophat, giga * mile), None, 0),
+                magnitude=18.5,
+                discovery=Discovery("Chadwick Trujillo and Michael Brown, of Caltech", 2002,
+                                    telescope = "Palomar Oschin Schmidt",
+                                    note = """2002 LM60, a.k.a. Quaoar
+
+Drs. Trujillo and Brown first observed this Kuiper Belt object, then (July 5,
+August 1) had the Hubble Space Telescope take a closer look, to determine true
+angular size, 40 * milli * arc.second.  They named it after the creator-god of
+the Tongva, the original inhabitants of the Los Angeles basin.\n"""))
+
+# Even further out: beyond Kuiper, in the "inner Oort Cloud"
+ape = Quantity(900, AU)
+ape.observe(130 * tera * metre)
+ape.observe(84 * giga * mile)
+peri = Quantity(76 + 14 * tophat, AU)
+Sedna = Planet('Sedna', Spheroid((950 + 300 * tophat) * mile,
+                                 # surface temperature is "about" -400 F.
+                                 temperature=Fahrenheit(-400 + 20 * tophat),
+                                 # it "likely rotates once every approximately 40 days"
+                                 #' suggesting that it's tidally locked to a moon
+                                 spin=Spin(Quantity(40 + 10 * tophat, day))),
+               # orbit comes as close as
+               Orbit(Sun,
+                     .5 * (ape.best + peri.best) + (ape.high - peri.low) * tophat,
+                     Spin(10.5 * kilo * year),
+                     perihelion=peri, apehelion=ape),
+               discovery=Discovery(
+    "Michael Brown (Caltech), Chadwick Trujillo (Gemini Observatory), David Rabinowitz (Yale)",
+    2004, telescope="Samuel Oschin", observatory="Palomar",
+    note="""2003 VB12, a.k.a. Sedna
+
+First identified as an 'inner' Oort cloud object in early November 2003, the
+astronomers subsequently traced it back to 2001 in archived data.
+Named after the Inuit goddess of the sea, from whom all sea creatures were
+created; she's said to live at the bottom of the Arctic Ocean.\n"""))
+# Also: Sedna, eccentric 10,500 year orbit, 8 to 84 giga miles radius (putting
+# it in Sun.Bode[9:13]); between 800 and 1100 miles across; may have a moon.
+del ape, peri
+
+del Orbit, Spin, Discovery, Sun, KLplanet, KLsurface, Spheroid, Planet
+
+_rcs_log = """
+$Log: Kuiper.py,v $
+Revision 1.1  2005-03-12 15:25:57  eddy
+Initial revision
+
+"""
---
+++ space/outer.py
@@ -0,0 +1,100 @@
+# -*- coding: iso-8859-1 -*-
+"""The Outer Planets of our Solar system
+
+$Id: outer.py,v 1.1 2005-03-12 15:21:04 eddy Exp $
+"""
+
+from basEddy.units import *
+from space.home import Sun, KLplanet, KLsurface
+from space.common import Orbit, Spin, Discovery
+tyr = 365.242198781 * day
+gassy = "H2, He, CH4, NH3, etc."
+
+Jupiter = KLplanet('Jupiter',
+                   KLsurface(11.19, 2.54, Spin(9 * hour + 50 * minute, 3.1),
+                             flattening = .0648),
+                   Orbit(Sun, (778.2 + .01 * tophat) * giga * metre,
+                         Spin(11.862 * year, 1.305), .0481),
+                   317.89, 1.33, atmosphere=gassy,
+                   discovery=Discovery("prehistoric", -1e-4,
+                                       etymology="""Latin: Jupiter
+
+The Romans, like the Greeks, named this planet (which we now know to be the
+largest and most massive) after the king of their gods.
+"""),
+                   aliases=("Zeus", "Jove"))
+Jupiter.mass.observe(1898.8e24 * kilogramme)
+Jupiter.surface.radius.observe(72 * mega * metre)
+Jupiter.surface.radius.observe(mega * (71.492 + .001 * tophat) * metre) # NASA
+Jupiter.surface.spin.period.observe(0.41354 * day)
+Jupiter.orbit.spin.period.observe(11.862 * tyr)
+Jupiter.orbit.radius.observe(giga * (778.30 + .1 * tophat) * metre) # NASA
+
+Saturn = KLplanet('Saturn',
+                  KLsurface(9.41, 1.07, Spin(10 * hour + 14 * minute, 26.7),
+                            flattening = .1076),
+                  Orbit(Sun, (1431 + .1 * tophat) * giga * metre,
+                        Spin(29.57 * year, 2.49), .051),
+                  95.18, .70, atmosphere=gassy,
+                  discovery=Discovery("prehistoric", -1e-4,
+                                      etymology="""Latin: Saturn
+
+Father of Jupiter (who killed him, quite likely by swallowing), known to the
+Greeks as Cronos, whose brothers and sisters were Titans, after whom many of
+Saturn's moons are named.
+
+Saturn is the most distant planet that can be observed from Earth with the naked
+eye.  It was the first gas giant whose rings got noticed.
+"""),
+                  aliases=("Cronos",))
+Saturn.mass.observe(568.5e24 * kilogramme)
+Saturn.surface.radius.observe(60.5 * mega * metre)
+Saturn.surface.radius.observe(mega * (60.268 + .001 * tophat) * metre) # NASA
+Saturn.surface.spin.period.observe(0.4375 * day)
+Saturn.orbit.spin.period.observe(29.458 * tyr)
+Saturn.orbit.radius.observe(giga * (1429.39 + .1 * tophat) * metre) # NASA
+
+Uranus = KLplanet('Uranus',
+                  KLsurface(3.98, .9, Spin(15 * hour + 34 * minute, 97.9),
+                            flattening = .03),
+                  Orbit(Sun, (2886 + .1 * tophat) * giga * metre,
+                        Spin(84.75 * year, .773), .047),
+                  14.5, 1.30, atmosphere=gassy, discovery=Discovery("Herschel", 1781))
+Uranus.mass.observe(86.625e24 * kilogramme)
+Uranus.surface.radius.observe(26.7 * mega * metre)
+Uranus.surface.radius.observe(mega * (25.559 + .001 * tophat) * metre) # NASA
+Uranus.surface.spin.period.observe(0.72 * day) # NASA
+Uranus.orbit.spin.period.observe(84.012 * tyr)
+Uranus.orbit.spin.period.observe(83.75 * year) # NASA (suspiciously 1 year off K&L)
+Uranus.orbit.radius.observe(giga * (2875.04 + .1 * tophat) * metre) # NASA
+
+Neptune = KLplanet('Neptune',
+                   KLsurface(3.81, 1.2, Spin(18 * hour + 26 * minute, 29.6),
+                             flattening = .026),
+                   Orbit(Sun, (4529 + .1 * tophat) * giga * metre,
+                         Spin(167 * year, 1.77), .007),
+                   17.24, 1.76, atmosphere=gassy,
+                   discovery=Discovery("Galle", 1846,
+                                       __doc__="""Neptune's discovery
+
+Two young mathematicians, Adams and le Verrier, independently predicted that
+there must be a planet further out than Uranus, to account for observed motions;
+they were mostly ignored, but le Verrier persuaded Galle to check.  On September
+23rd, 1846, Galle found Neptune pretty much exactly where he'd been told to
+look.  A mere 17 days later, Lassell found Triton orbiting it.\n"""))
+Neptune.mass.observe(102.78e24 * kilogramme)
+Neptune.surface.radius.observe(24.9 * mega * metre)
+Neptune.surface.radius.observe(mega * (24.764 + .001 * tophat) * metre) # NASA
+Neptune.surface.spin.period.observe(0.67 * day) # NASA
+Neptune.orbit.spin.period.observe(164.798 * tyr)
+Neptune.orbit.spin.period.observe(163.72 * year) # NASA
+Neptune.orbit.radius.observe(giga * (4504.45 + .1 * tophat) * metre) # NASA
+
+del Orbit, Spin, Discovery, Sun, KLplanet, KLsurface, tyr, gassy
+
+_rcs_log = """
+$Log: outer.py,v $
+Revision 1.1  2005-03-12 15:21:04  eddy
+Initial revision
+
+"""
