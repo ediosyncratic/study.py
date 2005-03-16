385fb3a76d64e743cf6287ff20a4854f75ef5280
Edward Welbourne <eddy@chaos.org.uk>
2005-03-16 23:23:05 +0000

    Moved Gliese710 from Kuiper, tidied up by adding Stjerne to package solstation data.

--- space/star.py
+++ space/star.py
@@ -2,22 +2,60 @@
 """Some data on stars.
 
 Sources:
+  http://www.solstation.com/stars.htm and links therefrom
+
   http://www.solstation.com/stars/alp-cent3.htm
   comes with good links, and yas more data I haven't finished plundering ;^)
 
-$Id: star.py,v 1.2 2005-03-14 00:01:01 eddy Exp $
+$Id: star.py,v 1.3 2005-03-16 23:23:05 eddy Exp $
 """
 
-from basEddy.units import tophat, kilo, mega, giga, micro, year, day
-from space.body import Object, Galaxy, Star
+from basEddy.units import Quantity, tophat, kilo, mega, giga, micro, year, day
+from space.body import Object, Star
 from space.common import Discovery, Spheroid, Spin, Orbit
-from space.home import MilkyWay, Sun, AU
+from space.home import Sun, AU
 
 class Group (Object): pass
 class Cluster (Group): pass
+def Stjerne(name, typ, dist, locn, mass, size, lum, spin=None,
+            Sp=Spin, Sf=Spheroid, Sol=Sun, ly=year.light, dy=day, S=Star, **what):
+    """Initialize a Star based (mostly) on solstation.com's data."""
+
+    if spin is None: skin = Sf(Sol.surface.radius * size)
+    else: skin = Sf(Sol.surface.radius * size, spin=Sp(dy * spin))
+
+    what.update({'type': typ, 'distance': dist * ly,
+                 'ICRS2000_0': locn, # 'ICRS 2000.0' data
+                 'mass': mass * Sol.mass,
+                 'surface': skin,
+                 'bright': lum * Sol.bright })
+
+    return apply(S, (name,), what)
+
+del Sun, Object, Star, day, Spheroid
+
+Gliese710 = Stjerne('Gliese 710', 'K5-M1 V', 63.0 + tophat * .1,
+                    '18:19:50.8-1:56:19.0',
+                    Quantity(.5 + .2 * tophat, best=.42), # but xs4all.n./~mke gave 1e5 * Earth.mass
+                    (4.2 + .1 * tophat) / 100, .67 + .1 * tophat, # 'possibly 67 percent'
+                    aliases=('NSV 10635', 'Gl 710', 'Hip 89825', 'BD-01 3474',
+                             'HD 168442', 'HD 168442', 'U449', 'Vys/McC 63'),
+                    __doc__ = """Gliese 710
+
+According to http://www.xs4all.nl/~mke/Gliese710.htm this is a red dwarf headed
+our way at 50,400 km/hr, 50 times the size of Earth, 100,000 times as massive
+and due to arrive in about 1.4 mega years.  However, solstation reports that
+astronomers don't expect it to disturb the Oort cloud enough to 'create a
+substantial increase in the long-period comet flux at Earth's orbit'.
+
+Apparently, we're also due (not quite so close, but nearer than Proxima
+Centauri, our current nearest neighbour) visits from Barnard's star (10,000
+years hence) and Alpha Centauri (A/B).\n""",
+                 # but http://www.solstation.com/stars2/gl710.htm
+                 # gives "within 1.1 ly (0.34 pc)"; i.e. c. 7e4 AU
+                 closestapproach = 4e4 * AU)
 
 AlphaCentauri = Group('Alpha Centauri',
-                      # locations are 'ICRS 2000.0'
                       aliases=('Rigel Centaurus',),
                       discovery=Discovery('prehistoric', (-2 + tophat * 3) * kilo,
                                           etymology="Arabic: Rigil Kentaurus = the foot of the Centaur",
@@ -25,41 +63,35 @@ AlphaCentauri = Group('Alpha Centauri',
 
 It's the fourth brightest star in the night sky as well as the brightest star in
 Constellation Centaurus; it's been known about for millennia.\n"""),
-                      A = Star('Alpha Centauri A', 'G2 V', 4.36,
-                               aliases=('Alf Cen A', 'HR 5459', 'Gl 559 A',
-                                        'Hip 71683', 'HD 128620', 'CP(D)-60 5483',
-                                        'SAO 252838', 'FK5 538', 'LHS 50'),
-                               discovery=Discovery('Nicholas Louis de La Caille', 1752,
-                                                   location='Cape Hope',
-                                                   story="""Discovery that Alpha Centauri was two stars
+                      A = Stjerne('Alpha Centauri A', 'G2 V', 4.36 + tophat * .01,
+                                  '14:39:36.5-62:50:02.3', 1.095 + .01 * tophat,
+                                  1.56 + .08 * tophat, 1.23 + .01 * tophat,
+                                  aliases=('Alf Cen A', 'HR 5459', 'Gl 559 A',
+                                           'Hip 71683', 'HD 128620', 'CP(D)-60 5483',
+                                           'SAO 252838', 'FK5 538', 'LHS 50'),
+                                  discovery=Discovery('Nicholas Louis de La Caille', 1752,
+                                                      location='Cape Hope',
+                                                      story="""Discovery that Alpha Centauri was two stars
 
 The Abbé [Abbot] Nicholas Louis de La Caille (1713-1762) was at the Cape of Good
 Hope, the southernmost point of Africa, in 1752 studying the stars of the
 southern hemisphere with just an half-inch (8x) refractor.  He noticed that
-Alpha Centauri was actually two stars.\n"""),
-                               location='14:39:36.5-62:50:02.3',
-                               bright=Sun.bright * (1.56 + .08 * tophat),
-                               mass=Sun.mass * (1.095 + .01 * tophat),
-                               surface=Spheroid(Sun.surface.radius * (1.23 + .01 * tophat))),
-                      B = Star('Alpha Centauri B', 'K0-1 V', 4.36,
-                               aliases=('Alf Cen B', 'HR 5460', 'Gl 559 B',
-                                        'Hip 71681', 'HD 128621', 'LHS 51'),
-                               location='14:39:35.1-60:50:13.8',
-                               mass=Sun.mass * (.907 + .001 * tophat),
-                               bright=Sun.bright * (.485 + .07 * tophat),
-                               surface=Spheroid(Sun.surface.radius * (.865 + .01 * tophat))),
-                      C = Star('Proxima Centauri', 'M5.5 Ve', 4.22,
-                               location='14:29:42.95-62:40:46.14',
-                               aliases=('V645 Centauri', 'Gl 551', 'Hip 70890', 'LHS 49',
-                                        'Alf Cen C'),
-                               mass=Sun.mass * (.123 + .01 * tophat),
-                               bright=Sun.bright * micro * (86.5 + 67 * tophat), # highly variable !
-                               age=giga * year * (5.5 + tophat),
-                               surface=Spheroid(Sun.surface.radius * (.145 + .01 * tophat),
-                                                spin=Spin(day * (31.5 + 3 * tophat))),
-                               discovery=Discovery('Robert Thorburn Ayton Innes', 1915,
-                                                   location='Cape Hope',
-                                                   story="""Proxima Centauri's discovery
+Alpha Centauri was actually two stars.\n""")),
+                      B = Stjerne('Alpha Centauri B', 'K0-1 V', 4.36 + tophat * .01,
+                                  '14:39:35.1-60:50:13.8', .907 + .001 * tophat,
+                                  .485 + .07 * tophat, .865 + .01 * tophat,
+                                  aliases=('Alf Cen B', 'HR 5460', 'Gl 559 B',
+                                           'Hip 71681', 'HD 128621', 'LHS 51')),
+                      C = Stjerne('Proxima Centauri', 'M5.5 Ve', 4.22 + tophat * .01,
+                                  '14:29:42.95-62:40:46.14', .123 + .01 * tophat,
+                                  micro * (86.5 + 67 * tophat), # highly variable !
+                                  .145 + .01 * tophat, 31.5 + 3 * tophat,
+                                  aliases=('V645 Centauri', 'Gl 551', 'Hip 70890', 'LHS 49',
+                                           'Alf Cen C'),
+                                  age=giga * year * (5.5 + tophat),
+                                  discovery=Discovery('Robert Thorburn Ayton Innes', 1915,
+                                                      location='Cape Hope',
+                                                      story="""Proxima Centauri's discovery
 
 Although Alpha Centauri was known to be double at least as early as 1752, the
 third member of the group was not discovered until 1915.  Like de La Caille
@@ -97,11 +129,14 @@ AlphaCentauri.C.orbit = Orbit(AlphaCentauri,
                               # Not sure what upper bound to guess, but lower bound is .59:
                               .8 + .42 * tophat)
 
-del MilkyWay, Sun, Object, Galaxy, Star
+del Stjerne, Discovery, Spin, Orbit, Quantity, tophat, kilo, mega, giga, micro, year, AU
 
 _rcs_log = """
 $Log: star.py,v $
-Revision 1.2  2005-03-14 00:01:01  eddy
+Revision 1.3  2005-03-16 23:23:05  eddy
+Moved Gliese710 from Kuiper, tidied up by adding Stjerne to package solstation data.
+
+Revision 1.2  2005/03/14 00:01:01  eddy
 Import/export clean-up.
 
 Initial Revision 1.1  2005/03/13 23:55:49  eddy
