e65ca159e5936f21b6a510dba4739fbce53107f2
Edward Welbourne <eddy@chaos.org.uk>
2005-04-17 14:42:57 +0000

    More experimentation.  Reorganized docs. Check-in to have a fall-back: I'm about to try trimming arithmetic tail-piece.

--- space/Bode.py
+++ space/Bode.py
@@ -1,68 +1,91 @@
 # -*- coding: iso-8859-1 -*-
-"""The auto-Bodalizer.
-
-Trying to invent a Titius-Bode sort of a sequence for an arbitrary family of
-bodies orbiting a common centre.  Bogus and experimental.
-
-$Id: Bode.py,v 1.4 2005-03-15 21:59:27 eddy Exp $
-"""
-
-from basEddy.lazy import Lazy
-
-class Bodalizer (Lazy):
-    """The Titius-Bode law, a.k.a. Bode's law:
+"""The Titius-Bode law, a.k.a. Bode's law:
 
 A mathematical formula which generates, with modest accuracy, the semimajor axes
 of the planets in order out from the Sun.  Write down the sequence
-        0, 3, 6, 12, 24, ...
+        0, 3, 6, 12, 24, 48, 96, ...
 and add 4 to each term:
-        4, 7, 10, 16, 28, ...
+        4, 7, 10, 16, 28, 52, 100, ...
 Then divide each term by 10.  This leaves you with the series
-        0.4, 0.7, 1.0, 1.6, 2.8, ...
-which is intended to give you the semimajor axes of the planets measured in
-astronomical units.
+        0.4, 0.7, 1.0, 1.6, 2.8, 5.2, 10.0, ...
+which reasonably approximate the semimajor axes of the planets, measured in
+astronomical units; more accurately they're
+    0.38692, 0.72300, 1, 1.52296, 5.1998, 9.556, 19.25, 30.18, 39.59
+but the last few of these (which are better modelled by an arithmetic
+progression in steps of 10 AU) weren't known when the pattern was first noticed.
 
 Bode's law had no theoretical justification when it was first introduced; it
-did, however, agree with the soon-to-be-discovered planet Uranus' orbit (19.2 AU
-actual; 19.7 AU predicted).  Similarly, it predicted a missing planet between
+did, however, agree with the soon-to-be-discovered planet Uranus' orbit (19.25
+AU actual; 19.6 AU predicted).  Similarly, it predicted a missing planet between
 Mars and Jupiter, and shortly thereafter the asteroids were found in very
 similar orbits (2.77 AU actual for Ceres; 2.8 AU predicted).  The series,
 however, seems to skip over Neptune's orbit.  The form of Bode's law (that is, a
 roughly geometric series) is not surprising, considering our theories on the
 formation of solar systems, but its particular formulation is thought of as
-coincidental.
+coincidental.  In particular, it is notable that the pattern from Saturn
+outwards is better modelled by an arithmetic series (in steps of 10 AU), whose
+next value inwards would be roughly zero, -0.46 AU.
 
 (That's from http://www.alcyone.com/max/physics/laws/b.html; while
 http://www.astropa.unipa.it/versione_inglese/Hystory/BODE'S_LAW.htm
 gives some history, as follows.)
 
-The law first appeared in 1766 in a translation, by Johann Daniel Titius of
+The law first appeared in 1766 in a translation, by Johann Daniel Titius von
 Wittenberg, of Contemplation de la Nature (1764), by the French natural
 philosopher Charles Bonnet.  Titius appears to have drawn the law from private
-correspondence with others.  Bode repeated it in a foot-note, then went on to
-become a professional astronomer, so everyone cited him as source ...
+correspondence with others.  Johann Bode repeated it in a foot-note, then went
+on to become a professional astronomer, so everyone cited him as source ...
 
 Herschel's discovery (March 1781) of Uranus encouraged various astronomers to
 search for a planet in the gap between Mars and Jupiter, where the law predicts
 a planet at 2.8 AU; in 1800, a team set up an international collaboration;
 starting on the first day of the next year, Piazzi made the first of a series of
 observations of what he soon realised was such a missing planet, which he named
-Ceres Ferdinandea.\n"""
+Ceres Ferdinandea.
+
+$Id: Bode.py,v 1.5 2005-04-17 14:42:57 eddy Exp $
+"""
+
+from basEddy.lazy import Lazy
+
+class Bodalizer (Lazy):
+    """The auto-Bodalizer.
+
+An instance of this class tries to automatically select three constants zero,
+unit and base for which a family of orbits' radii have values close to
+        zero + unit * base**i
+for various i.  It allows i to change in steps other than 1, but should (though
+it presently does not) try to ensure that such exceptions are rare.
+
+Constructor takes a sequence of space.body.Object instances, selects a sub-set
+of them (the instances of the earliest of Planet, Planetoid, Body or Object to
+have several instances in the sequence) and records the orbital radii of this
+sub-set.  When it comes to need values for zero, unit and base it does the
+necessary computation to match this sequence of radii to the above pattern.
+
+Public attributes:
+ * zero, unit, base -- the defining attributes mentioned above.
+
+Public methods:
+ * use(zero, unit, base) -- over-ride the given attributes
+ * index(radius) -- i = log((radius - zero) / unit) / log(base)
+"""
 
     def __init__(self, seq):
         # First identfy your demos ...
         plenty, k = len(seq), 3
         while k * k < plenty: k = 1 + k
         plenty = k
-        for k in self.bodytypes:
+        for k in self._bodytypes:
             row = filter(lambda x, k=k: isinstance(x, k), seq)
             if len(row) > plenty: break
         self.__seq = map(lambda x: x.orbit.radius, row)
 
-    def _lazy_get_bodytypes_(self, ignored, seq=[]):
+    def _lazy_get__bodytypes_(self, ignored, seq=[]):
         if seq: return tuple(seq)
         from body import Planet, Planetoid, Body, Object
         seq[:] = [ Planet, Planetoid, Body, Object ]
+        return tuple(seq)
 
     def __len__(self): return len(self.__seq)
     def __getitem__(self, key):
@@ -74,6 +97,8 @@ Ceres Ferdinandea.\n"""
     # Auto-detection of zero, unit and base:
 
     from basEddy.quantity import Quantity, tophat, tera
+    # NB: Any scalar Quantity has attributes exp and log (among others) we can exploit ;^)
+
     def median(seq, span=1, Q=Quantity, unit=tophat): # tool func
         seq.sort()
         i, b = divmod(len(seq), 2)
@@ -86,13 +111,33 @@ Ceres Ferdinandea.\n"""
 
     from basEddy.SI import metre
     Unit = Quantity(tera * metre / 7) # Arbitrary Unit of length (approximates the AU)
-    del Quantity, tophat, tera, metre
+    del tophat, tera, metre
 
-    import math
-    def _lazy_get_unit_(self, ig, ln=math.log, mid=median, AU=Unit):
+    def use(self, zero=None, unit=None, base=None, Q=Quantity):
+        if zero is None:
+            try: del self.zero
+            except AttributeError: pass
+        else: self.zero = zero
+
+        if unit is None:
+            try: del self.unit
+            except AttributeError: pass
+        else: self.unit = unit
+
+        if base is None:
+            try: del self.base
+            except AttributeError: pass
+            try: del self.__base
+            except AttributeError: pass
+        else:
+            self.unit # Force lazy evaluation so we can blot out its __base ...
+            self.base = Q(base)
+            self.__base = self.base.log
+
+    def _lazy_get_unit_(self, ig, mid=median, AU=Unit):
         row = map(lambda x, z=self.zero: x - z, filter(lambda x, z=self.zero: x > z, self.__seq))
         # That forced computation of zero, making base available ...
-        offs = map(lambda x, log=ln, b=self.__base, u=AU: ((x/u).evaluate(log)/b) % 1, row)
+        offs = map(lambda x, b=self.__base, u=AU: ((x/u).log/b) % 1, row)
         frac = offs[:]
         frac.sort()
         gaps = [ frac[0] + 1 - frac[-1] ] + map(lambda x, y: y - x, frac[:-1], frac[1:])
@@ -105,22 +150,23 @@ Ceres Ferdinandea.\n"""
             i = i - 1
             r = offs[i]
             if r > dim: r = r - 1
-            n = (row[i] / AU).evaluate(ln) / self.__base - r
+            n = (row[i] / AU).log / self.__base - r
             ind.insert(0, int(n.best))
         # but that leaves an arbitrary offset in ind.
         offs = map(lambda n, i: n - i - 1, ind, range(len(ind)))
-        print 'offsets:', offs
+        # print 'offsets:', offs
         offs.sort()
         ind = map(lambda i, n=offs[len(offs)/3]: i - n, ind)
         return mid(map(lambda r, i, b=self.base: r / b**i, row, ind))
 
     del median
 
-    def _lazy_get_base_(self, ignored, e=math.exp):
+    def _lazy_get_base_(self, ignored):
         self.zero # force evaluation so we compute __base
-        return e(self.__base)
+        return self.__base.exp
 
-    def __spread(self, z, e=math.exp, ln=math.log, AU=Unit):
+    import math
+    def __spread(self, z, e=math.exp, AU=Unit):
         # icky complex-to-complex used in hunting good values for zero and base
         zero, b = e(z.imag) * AU, e(z.real)
         # it may be prudent to frob oz ...
@@ -128,7 +174,7 @@ Ceres Ferdinandea.\n"""
 
         row = filter(lambda x, z=zero: x > z, self.__seq)
         if b > 0 and len(row) > 1:
-            row = map(lambda x, log=ln, b=b, u=AU: ((x/u).evaluate(log)/b) % 1, row)
+            row = map(lambda x, b=b, u=AU: ((x/u).log/b) % 1, row)
             row.sort()
             gap = max([ row[0] + 1 - row[-1] ] + map(lambda x, y: y - x, row[:-1], row[1:]))
             span, oz = ((1 - gap) / len(row)).best, (oz / len(row)).best
@@ -142,35 +188,39 @@ Ceres Ferdinandea.\n"""
 
     from search import Search
 
-    def _lazy_get_zero_(self, ignored, S=Search, ln=math.log, e=math.exp, AU=Unit):
+    def _lazy_get_zero_(self, ignored, S=Search, e=math.exp, AU=Unit, Q=Quantity):
         # Mercury's orbit as zero:
         z = self.__seq[0]
         # Saturn vs. Venus gives median log(ratio)/(difference in index)
-        b = ((self.__seq[5] - z) / (self.__seq[1] - z)).evaluate(ln) * .2
+        try: b = ((self.__seq[5] - z) / (self.__seq[1] - z)).log * .2
+        except IndexError:
+            b = Q(2).log # fall back on 2
 
-        guess = float(b.evaluate(ln).best) + 1j * float((z / AU).evaluate(ln).best)
+        guess = float(b.log.best) + 1j * float((z / AU).log.best)
         hunt = S(self.__spread, guess, stride=.1)
         hunt.rummage()
 
         z = hunt.best[0]
-        self.__base = e(z.real)
+        self.__base = Q(e(z.real))
         return e(z.imag) * AU
 
-    del Search, Unit
+    del Quantity, Search, Unit, math
 
-    def index(self, radius, ln=math.log):
+    def index(self, radius):
         r = (radius - self.zero) / self.unit
         if r.low < .0001: return 0 # avoid domain errors and hugely -ve answers ...
         # (while incidentally being polite to Mercury)
-        return r.evaluate(ln) / self.__base
-
-    del math
+        return r.log / self.__base
 
 del Lazy
 
 _rcs_log = """
 $Log: Bode.py,v $
-Revision 1.4  2005-03-15 21:59:27  eddy
+Revision 1.5  2005-04-17 14:42:57  eddy
+More experimentation.  Reorganized docs.
+Check-in to have a fall-back: I'm about to try trimming arithmetic tail-piece.
+
+Revision 1.4  2005/03/15 21:59:27  eddy
 Revised Bode's constructor to take the one arg it's going to get ...
 
 Revision 1.3  2005/03/13 18:45:49  eddy
