b3ec45ea72f3b216f3b302a97a34aecf4b076aed
Edward Welbourne <eddy@chaos.org.uk>
2005-12-31 16:59:10 +0000

    No need to search: general solution known :-)

--- maths/pythagorean.py
+++ maths/pythagorean.py
@@ -1,40 +1,26 @@
-"""Search for pythagorean triangles.
+"""Generate pythagorean triangles.
 
-For each natural i,
-        (2*i+1)**2 = 4*i*(i+1) +1 = (2*i*(i+1) +1)**2 -(2*i*(i+1))**2
-yields
-        (2*i+1)**2 +(2*i*(i+1))**2 = (2*i*(i+1) +1)**2
-hence a (2*i+1):(2*i*(i+1)):(2*i*(i+1)+1) pythagorean triangle.
-Any scaling of a pythagorean triangle is likewise pythagorean.
-
-That's the j=1 case of:
-        (n+j)**2 - n**2 = 2*j*n +j**2
-and whenever the latter is a perfect square we get another p.t.
+For each naturals i, j with i > j, the triangle with sides
+        2*(i*(i+1) +(j+1)*j) +1
+        2*(i*(i+1) -(j+1)*j)
+        (2*i+1)*(2*j+1)
+is a right-angle triangle, as may be verified by summing the squares of
+the later two and comparing to the square of the first
+(see http://www.chaos.org.uk/~eddy/math/pythagoras.html#Whole).
 """
 
-_rcs_id = "$Id: pythagorean.py,v 1.1 2005-12-31 16:48:44 eddy Exp $"
+_rcs_id = "$Id: pythagorean.py,v 1.2 2005-12-31 16:59:10 eddy Exp $"
+
+def whole(i, j):
+    ans = ( 2*(i*(i+1) +(j+1)*j) +1, 2*(i*(i+1) -(j+1)*j), (2*i+1)*(2*j+1) )
+    assert ans[1]**2 +ans[2]**2 == ans[0]**2
+    return ans
 
 from natural import hcf
-
-def hunt((n,i,j)=(0,0,0)):
-    # hypotenuse n, longest other side i
-    if n < i: i, n = n, i
-    while 1:
-        i = 1 + i
-        if i >= n:
-            # print 'Done:', n,
-            n, i = 1 + n, 1
-            while 2*i*i < n*n: i = 1 + i
-            i = i - 1
-            # print '\tNext:', n, i
-        else:
-            # check whether n**2 -i**2 is a perfect square
-            gap, j = (n-i) * (n+i), 1
-            while j*j < gap: j = 1 + j
-            if j*j == gap and hcf(n, i, j) == 1:
-                assert n*n == i*i + j*j
-                return n, i, j
-            # print 'Failed:', n, i
+def coprime(i, j):
+    h, a, c = whole(i, j)
+    f = hcf(h, a, c)
+    return h/f, a/f, c/f
 
 pythanim = """
 For the pythagorean animation, I need the size-lists of the two small squares,
@@ -55,10 +41,12 @@ So what we really need is three lists n, s, c, of equal length N
    * c must be a good approximation to cos(n) and
    * s*s + c*c must be a very good approximation to 1.
 """
+
 
 _rcs_log = """
  $Log: pythagorean.py,v $
- Revision 1.1  2005-12-31 16:48:44  eddy
- Initial revision
+ Revision 1.2  2005-12-31 16:59:10  eddy
+ No need to search: general solution known :-)
 
+ Initial Revision 1.1  2005/08/18 16:48:44  eddy
 """
