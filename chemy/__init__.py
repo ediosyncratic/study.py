dd4d5771bfaa878abac76837414ee2f9705dc1d3
Edward Welbourne <eddy@chaos.org.uk>
2007-03-24 14:59:34 +0000

    Over-view of what's available.

---
+++ chemy/__init__.py
@@ -0,0 +1,13 @@
+"""Fundamental physics through to chemistry.
+
+ atomic -- modelling the electron orbitals of atoms
+ decay -- modelling how things fall apart
+ element -- the chemical elements and their isotopes
+ particle -- fundamental particles of physics
+ physics -- properties of the fundamental fields of physics
+ planck -- Planck's units
+ substance -- properties of some substances
+ thermal -- black-body radiator
+
+$Id: __init__.py,v 1.1 2007-03-24 14:59:34 eddy Exp $
+"""
