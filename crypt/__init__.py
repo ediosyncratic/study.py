49f4fdf1fc40d222ac3360b3db46da313670912d
Edward Welbourne <eddy@chaos.org.uk>
2007-03-24 16:04:35 +0000

    Over-view of this part of the study package.

---
+++ crypt/__init__.py
@@ -0,0 +1,11 @@
+"""Encoding and decoding tests in various ways.
+
+Fragments available:
+ base -- representing numbers as strings in various bases
+ dequote -- undoing the 'quoted-printable' encoding used in e-mails
+ Huffman -- text-compressor using the Huffman coding
+ morse -- using dots and dashes to represent text
+ radix050 -- an archaic base-fourty text encoding
+
+$Id: __init__.py,v 1.1 2007-03-24 16:04:35 eddy Exp $
+"""
