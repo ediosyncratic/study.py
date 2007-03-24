c8b99c5b24a03a5a69393d4960a3a80f21c314fc
Edward Welbourne <eddy@chaos.org.uk>
2007-03-24 16:55:29 +0000

    Over-view of this part of the study package.

---
+++ value/__init__.py
@@ -0,0 +1,13 @@
+"""Representing numbers in some non-standard ways.
+
+Fragments:
+  bigfloat -- extends the range of floating-point values
+  lazy -- helper classes for lazy evaluation
+  object -- provides a generic Object class
+  quantity -- describes a value with units of measurement
+  sample -- describes a number with uncertain value
+  SI -- the base units of SI
+  units -- miscelaneous other units
+
+$Id: __init__.py,v 1.1 2007-03-24 16:55:29 eddy Exp $
+"""
