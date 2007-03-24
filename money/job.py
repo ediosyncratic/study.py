929c431705d194526a79abb254ef705d4de14062
Edward Welbourne <eddy@chaos.org.uk>
2007-03-24 16:41:09 +0000

    Adjusted imports to new structure, tidy up RCS tags. Moved money units out to study.money and single instance elsewhere.

--- money/job.py
+++ money/job.py
@@ -1,35 +1,10 @@
 # -*- coding: iso-8859-1 -*-
-"""Units and related things related to money and a job.
+"""Descriptin of a Job in terms of time and money.
 
-$Id: job.py,v 1.2 2006-03-24 08:32:10 eddy Exp $
+$Id: job.py,v 1.3 2007-03-24 16:41:09 eddy Exp $
 """
+from value.lazy import Lazy
 
-from units import *
-
-quid = base_unit('£', 'Pound Sterling',
-		 """The base unit of British currency.
-
-Used to be 20 shillings (21 shillings made a Guinea); each shilling was 12
-pence, each penny was four farthings.  A florin was two shillings; a crown was
-five.  Apparently a pound was also called a sovereign.  HTML supports character
-entity &sterling; for the Pound Sterling.
-
-To properly handle money within a system of units, I need support for variation
-in time and space (conversion factors between different currencies vary with
-time; and you'll get different exchange rates from different trading partners).
-Then again, conversion factors between systems of units also show similar
-variation - contrast the different nations' archaic units of length, and notice
-how units of volume got re-defined by assorted legislative acts over the years.
-""")
-
-# It's clearly inadequate to treat money units as approximate multiples of one
-# another: each is an exact unit in its place, it's only the conversion between
-# them that's approximate.
-krone = quid / (12 + 2 * tophat)
-
-from lazy import Lazy
-
-# temp class; one instance, working - see below, describes time and pay
 class Job (Lazy):
     # Should probably inherit dailyhours, public, weeklydays from
     # per-jurisdiction base object.
@@ -93,18 +68,3 @@ class Job (Lazy):
     def _lazy_get_annual_(self, ignored): return self.rate * self.year
     def _lazy_get_monthly_(self,   ignored): return self.annual / 12
     def _lazy_get_quarterly_(self, ignored): return self.annual / 4
-
-working = Job(412572 * krone, year, 26)
-# 2005: 369612 * krone, year
-# 2002: 28000 * quid, year, 7.5, 8, 5
-del Job
-
-_rcs_log = """
- $Log: job.py,v $
- Revision 1.2  2006-03-24 08:32:10  eddy
- krone not kroner
-
- Revision 1.1  2006/03/24 08:28:37  eddy
- Initial revision
-
-"""
