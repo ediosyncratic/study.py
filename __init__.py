775ee329431a56b189c387cc41ba1c8377ce5963
Edward Welbourne <eddy@chaos.org.uk>
2007-03-24 13:37:46 +0000

    Quick over-view of the parts

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""Tools and data for studying various scientific and mathematical topics.
+
+Sub-packages:
+
+ chemy -- fundamental physics through to chemistry
+ code  -- tools for encoding and decoding
+ maths -- assorted mathematical tools
+ space -- the solar system and some of the rest of the universe
+ value -- imprecise numbers and units of measurement, combined
+
+$Id: __init__.py,v 1.1 2007-03-24 13:37:46 eddy Exp $
+"""
