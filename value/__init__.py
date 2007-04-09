460e0a3109746996c073cff00e4ac907a8c7dcba
Edward Welbourne <eddy@chaos.org.uk>
2007-04-09 00:42:38 +0000

    Split off most of the archaic mess from units.py to archaea.py

--- value/__init__.py
+++ value/__init__.py
@@ -1,13 +1,14 @@
 """Representing numbers in some non-standard ways.
 
 Fragments:
+  archaea -- archaic and/or silly units
   bigfloat -- extends the range of floating-point values
   lazy -- helper classes for lazy evaluation
   object -- provides a generic Object class
   quantity -- describes a value with units of measurement
   sample -- describes a number with uncertain value
   SI -- the base units of SI
-  units -- miscelaneous other units
+  units -- other vaguely sensible units
 
-$Id: __init__.py,v 1.1 2007-03-24 16:55:29 eddy Exp $
+$Id: __init__.py,v 1.2 2007-04-09 00:42:38 eddy Exp $
 """
