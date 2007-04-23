3541858a353f44b1b2e6bfd5d8b3621c34ea6d52
Edward Welbourne <eddy@chaos.org.uk>
2007-04-23 04:25:28 +0000

    Added _TODO to hold thoughts about potential improvements.

--- maths/primes.py
+++ maths/primes.py
@@ -19,25 +19,56 @@ See also generic integer manipulators in natural.py, combinatorial tools in
 permute.py and polynomials in polynomial.py: some day, it'd be fun to do some
 stuff with prime polynomials ...
 
-$Id: primes.py,v 1.8 2007-04-22 20:32:59 eddy Exp $
+$Id: primes.py,v 1.9 2007-04-23 04:25:28 eddy Exp $
 """
 
-checking = None
+_TODO = """Various potential improvements
+
+ * Iterator support:
+   - Support interface in existing class.
+   - By providing an iterator class.
+   - Use this iterator class in the computation of new primes.
+
+ * Cache improvements:
+   - Ensure that all uses load from cache in preference to working out afresh
+     (current .grow()-based approach doesn't).
+   - Change naming to use hex in file-names, varying first letter each time we
+     add a digit (so ls gets order right); use 'hidden' cache-dir.
+   - Change format to sequence of [ 1, 7, 11, 13, 17, 19, 23, 29 ] octets to
+     limit growth in file size (change init to [2, 3, 5], to go with that);
+     this'll make a bigger block-size practical.
+   - Think in terms of writing a .so module to implement crucial parts, when
+     implementing octet format.
+   - Turn primary object into a holder for objects describing sub-ranges; load
+     and unload these as needed, to limit how much is held in memory; iterators
+     need to remember their positions in terms of chunk index and offset, since
+     chunk may get unloaded between next()s.
+   - Support for list of read-only caches from which to borrow data; and (at
+     least initially) an 'old-style cache dir' so we can digest old data into
+     new cache format.
+
+ * Miscellaneous
+   - After exhausting factorise()'s trawl through known primes, try around the
+     number's (approximate) square root (see 'Conjecture').
+"""
+
 _aside_plus = """When is pow(2,n)+1 a prime ?
 
-when n is in (1,2,3,4,8,16) and then no more, at least as far as 42.
+when n is in (1,2,4,8,16) and then no more, at least as far as 42.
 Note that pow(2,32)+1 is 641*6700417
 """
 
 _aside_minus = """When is pow(2,n)-1 a prime ?
 
 Never with n even; and n=1 gives 1.  When n is in (3,5,7,13,17,19,31) and
-then no more, at least as far as 47.  Observe 31=pow(2,5)-1, 17=pow(2,4)+1,
+then no more, at least as far as 60.  Observe 31=pow(2,5)-1, 17=pow(2,4)+1,
 7=pow(2,3)-1, 5=pow(2,2)+1, with pow(2,1)-1=1.
 
 So when is pow(2, pow(2,i)+pow(-1,i))-1 a prime ?  For i=1 we get 1 which
-we ignore, then for i in (2,3,4,5) we get primes.  For i=6 I get a segfault.
+we ignore, then for i in (2,3,4,5,6) we get primes.
 """
+
+checking = None
 
 class lazyTuple:
     """Evaluation on demand of a (possibly infinite) tuple.
@@ -351,7 +382,8 @@ class _Prime(lazyTuple):
 		break
 
             # Conjecture: hunting outwards from approximately sqrt(num), rather
-            # than up from _ask, might be quicker.
+            # than up from _ask, might be quicker.  Try hunting down from first
+            # power of 2 whose square exceeds num.
 
 	    # Now go hunting for primes until the job is done:
 	    while num > 1:
