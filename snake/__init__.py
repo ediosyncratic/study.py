1e70316a0b5fb04492378f76dd7045b8bcfe2773
Edward Welbourne <eddy@chaos.org.uk>
2008-05-12 06:03:47 +0000

    Added prodict.

--- snake/__init__.py
+++ snake/__init__.py
@@ -2,7 +2,11 @@
 
 Modules:
  lazy -- support for lazy attribute look-up
+ prodict -- a dict modelling a product, with value as exponent for each key
+Sub-package:
+ endless -- implementing infinite lists, optionally with cacheing
 
- $Id: __init__.py,v 1.1 2007-06-03 16:52:54 eddy Exp $
+ $Id: __init__.py,v 1.2 2008-05-12 06:03:47 eddy Exp $
 """
 import lazy
+import prodict
