d2b3d97eb99fac0a55a78211712f377317a99690
Edward Welbourne <eddy@chaos.org.uk>
2008-05-12 09:14:41 +0000

    Imported basEddy.tools as error, row and show.

--- snake/__init__.py
+++ snake/__init__.py
@@ -1,12 +1,18 @@
 """General pythonic helper infrastructure.
 
 Modules:
+ error -- finding out about exceptions
  lazy -- support for lazy attribute look-up
- prodict -- a dict modelling a product, with value as exponent for each key
+ prodict -- dictionary of factors with multiplicities
+ row -- miscellaneous functions on sequences
+ show -- a pretty-printer for lists of strings
 Sub-package:
  endless -- implementing infinite lists, optionally with cacheing
 
- $Id: __init__.py,v 1.2 2008-05-12 06:03:47 eddy Exp $
+ $Id: __init__.py,v 1.3 2008-05-12 09:14:41 eddy Exp $
 """
+import error
 import lazy
 import prodict
+import row
+import show
