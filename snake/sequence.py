f17542db0662d471529b94f59e8ae106aaa46e97
Edward Welbourne <eddy@chaos.org.uk>
2008-06-08 15:07:10 +0000

    Added Ordered, refugee from maths/prime/scrap/

--- snake/sequence.py
+++ snake/sequence.py
@@ -1,6 +1,6 @@
 """Assorted classes relating to sequences.
 
-$Id: sequence.py,v 1.9 2008-06-05 07:26:39 eddy Exp $
+$Id: sequence.py,v 1.10 2008-06-08 15:07:10 eddy Exp $
 """
 
 class Tuple (object):
@@ -64,8 +64,7 @@ class Tuple (object):
 class Iterable (object):
     """Mix-in class to extend iterables in some handy ways.
 
-    Implements map, reduce, sum, product and filter as methods of the iterable.
-    """
+    Implements map, reduce, sum, product and filter as methods of the iterable.\n"""
     def map(self, func):
         ans = []
         for p in self:
@@ -94,3 +93,64 @@ class WrapIterable (Iterable):
     def __init__(self, seq): self.__seq = seq
     def __iter__(self): return iter(self.__seq)
     def __getattr__(self, key): return getattr(self.__seq, key)
+
+class Ordered (list):
+    """An ordered set.
+
+    Like a list except that you don't get to chose where in it to put each
+    entry; and duplication is silently ignored.  Entries must support comparison
+    with one another; they shall be kept in increasing order.\n"""
+
+    __upinit = list.__init__
+    def __init__(self, val=None):
+        self.__upinit(val or [])
+
+    def __locate(self, value):
+        """Where does value belong in this list ?
+
+        If value is equal to some entry in this list, return the index of that
+        entry; otherwise, return the index at which it should be inserted.  A
+        return of -1 means 'after the end'.\n"""
+        if not self or self[0] > value: return 0
+        if self[-1] < value: return -1 # magic token for "after end"
+        lo, hi = 0, len(self) - 1
+        while hi > 1 + lo:
+            mid, ig = divmod(lo + hi, 2)
+            if self[mid] > value: hi = mid
+            elif self[mid] < value: lo = mid
+            else: return mid
+
+        return hi
+
+    def index(self, value):
+        ind = self.__locate(value)
+        if ind < 0 or value != self[ind]:
+            raise ValueError('Not in list', value)
+        return ind
+
+    __listins = list.insert
+    __listapp = list.append
+    def append(self, value):
+        # Insert in correctly-ordered position.
+        at = self.__locate(value)
+        if at < 0: self.__listapp(value)
+        elif self[at] == value: return False
+        else: self.__listins(at, value)
+        return True
+
+    insert = append
+
+    def prune(self, upto):
+        """Discard all entries <= upto.
+
+        Returns the discarded entries.\n"""
+        at = self.__locate(upto)
+        if at < 0:
+            was = self[:]
+            del self[:]
+        else:
+            if self[at] == upto: at += 1
+            was = self[:at]
+            if at > 0: del self[:at]
+
+        return was
