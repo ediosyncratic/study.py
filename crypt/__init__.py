8105c6ca60656210886f9c0af561d4f0d449c59e
Edward Welbourne <eddy@chaos.org.uk>
2008-06-19 07:53:09 +0000

    Add the English letter frequencies mapping from David's book.

--- crypt/__init__.py
+++ crypt/__init__.py
@@ -7,8 +7,21 @@ Fragments available:
  morse -- using dots and dashes to represent text
  radix050 -- an archaic base-fourty text encoding
 
+Attribute:
+ english -- letter frequencies in English text
+
 See also:
  urllib -- standard python module, provides {un,}quote{,_plus} for %xx {en,de}coding.
 
-$Id: __init__.py,v 1.2 2007-12-02 21:05:23 eddy Exp $
+$Id: __init__.py,v 1.3 2008-06-19 07:53:09 eddy Exp $
 """
+
+# Taken from Information Theory, Inference, and Learning Algorithms (D.J.C. MacKay) p.100:
+english = {
+    ' ': 0.1928, 'e': 0.0913, 't': 0.0706, 'o': 0.0689,
+    'i': 0.0599, 'n': 0.0596, 'a': 0.0575, 's': 0.0567, 'r': 0.0508,
+    'l': 0.0335, 'u': 0.0334, 'h': 0.0313,
+    'd': 0.0285, 'c': 0.0263, 'm': 0.0235,
+    'p': 0.0192, 'f': 0.0173, 'y': 0.0164, 'g': 0.0133, 'b': 0.0128, 'w': 0.0119,
+    'k': 0.0084, 'x': 0.0073, 'v': 0.0069,
+    'q': 0.0008, 'z': 0.0007, 'j': 0.0006 }
