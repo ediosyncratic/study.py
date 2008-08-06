8a0615a252355e22d051b514f7b73aa38934de28
Edward Welbourne <eddy@chaos.org.uk>
2008-08-06 06:37:00 +0000

    Sub-module documentation.

---
+++ cache/__init__.py
@@ -0,0 +1,9 @@
+"""Assorted generic cache functionality.
+
+Provides:
+  property -- cached attributes, computed on depand
+  weak -- weakly remembered attributes and tuple or mapping entries
+  whole -- saving data about integers to disk
+
+$Id: __init__.py,v 1.1 2008-08-06 06:37:00 eddy Exp $
+"""
