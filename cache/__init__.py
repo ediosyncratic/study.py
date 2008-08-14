9720037e6afdd591682ab6ef692d83fba5027eaf
Edward Welbourne <eddy@chaos.org.uk>
2008-08-14 19:15:38 +0000

    Added lockdir, improved docs.

--- cache/__init__.py
+++ cache/__init__.py
@@ -1,9 +1,10 @@
 """Assorted generic cache functionality.
 
 Provides:
+  lockdir -- directory locking used by whole
   property -- cached attributes, computed on depand
-  weak -- weakly remembered attributes and tuple or mapping entries
-  whole -- saving data about integers to disk
+  weak -- weakly remembering things you can compute at will
+  whole -- saving, to disk, data about integer-bounded ranges of the number line
 
-$Id: __init__.py,v 1.1 2008-08-06 06:37:00 eddy Exp $
+$Id: __init__.py,v 1.2 2008-08-14 19:15:38 eddy Exp $
 """
