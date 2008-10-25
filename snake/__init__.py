42bae9c9aa6687ec2d2521ada62f5c32df5e7950
Edward Welbourne <eddy@chaos.org.uk>
2008-10-25 09:38:43 +0000

    Document and import the modules added since May.

--- snake/__init__.py
+++ snake/__init__.py
@@ -2,17 +2,25 @@
 
 Modules:
  error -- finding out about exceptions
+ infinite -- diverse representations of infinite values
  lazy -- support for lazy attribute look-up
  prodict -- dictionary of factors with multiplicities
+ property -- enhanced properties
+ regular -- arithmetic sequences that work like slice
  row -- miscellaneous functions on sequences
+ sequence -- base-classes for sequence types and iterables
  show -- a pretty-printer for lists of strings
 Sub-package:
  endless -- implementing infinite lists, optionally with cacheing
 
- $Id: __init__.py,v 1.3 2008-05-12 09:14:41 eddy Exp $
+ $Id: __init__.py,v 1.4 2008-10-25 09:38:43 eddy Exp $
 """
 import error
+import infinite
 import lazy
 import prodict
+import property
+import regular
 import row
+import sequence
 import show
