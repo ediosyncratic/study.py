bba98d66896b6a8071a4e60b4fb3a341a33ffb74
Edward Welbourne <eddy@chaos.org.uk>
2008-11-21 08:13:50 +0000

    Initial outline of purpose.

---
+++ parse/__init__.py
@@ -0,0 +1,10 @@
+"""Parsers and serialisers.
+
+Also objects representing files on disk, in such a way as to facilitate
+structured change to those objects in between their construction by parsing and
+their subsequent serialization in the course of saving to disk.  See also the
+tools in ../cache/whole.py for the case where the on-disk format is python
+modules.
+
+$Id: __init__.py,v 1.1 2008-11-21 08:13:50 eddy Exp $
+"""
