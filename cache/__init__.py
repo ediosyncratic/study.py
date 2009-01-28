24f3603307e6a662d10e5318a38061908b8bf5df
Edward Welbourne <eddy@chaos.org.uk>
2009-01-28 08:37:53 +0000

    Document mapping and actually import stuff.

--- cache/__init__.py
+++ cache/__init__.py
@@ -2,9 +2,15 @@
 
 Provides:
   lockdir -- directory locking used by whole
+  mapping -- a lazily-populated dictionary, LazyDict
   property -- cached attributes, computed on depand
   weak -- weakly remembering things you can compute at will
   whole -- saving, to disk, data about integer-bounded ranges of the number line
 
-$Id: __init__.py,v 1.2 2008-08-14 19:15:38 eddy Exp $
+$Id: __init__.py,v 1.3 2009-01-28 08:37:53 eddy Exp $
 """
+import lockdir
+import mapping
+import property
+import weak
+import whole
