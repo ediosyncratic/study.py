d9d0b303a0afcdabc7fccf1f2bbc0c6dd1511ae3
Edward Welbourne <eddy@chaos.org.uk>
2010-01-02 00:06:33 +0000

    Reworked to use class methods built on Permutation.all instead of Iterator.

--- maths/queens.py
+++ maths/queens.py
@@ -12,16 +12,17 @@ allowing black and white squares to swap), one of which is symmetric under a
 half turn of the board: [2, 4, 1, 7, 0, 6, 3, 5].
 
 The problem naturally generalizes to other sizes than 8, although 8 is the
-natural size for a standard chess board.  This module provides a plain Iterator
-over all solutions of a given size and a Unique iterator which skips equivalent
-solutions: two solutions are deemed equivalent if some symmetry of the chess
-board maps one onto another.
+natural size for a standard chess board.  This module provides a class Solution,
+which extends permute.Permutation; adding a .solve(size) iterator to iterate
+over all solutions of the given size; and adding a .unique(size) to do the same
+but skip equivalent solutions: two solutions are deemed equivalent if some
+symmetry of the chess board maps one onto another.
 
-$Id: queens.py,v 1.8 2009-03-18 09:13:33 eddy Exp $
+$Id: queens.py,v 1.9 2010-01-02 00:06:33 eddy Exp $
 """
 
-import permute
-class Solution(permute.Permutation):
+from permute import Permutation
+class Solution (Permutation):
     def __repr__(self):
         try: ans = self.__repr
         except AttributeError:
@@ -29,7 +30,7 @@ class Solution(permute.Permutation):
                 lambda i, n=len(self)-1: ' ' * i + '#' + ' ' * (n-i), self))
         return ans
 
-    def peaceful(self):
+    def __peaceful(self):
         i = len(self) - 1
         while i:
             j, n = i, self[i]
@@ -40,32 +41,38 @@ class Solution(permute.Permutation):
             i -= 1
         return True
 
-def Iterator(size=8):
-    """Iterates over all solutions to the `n queens' problem.
+    @classmethod
+    def solve(cls, size):
+        """Iterates over all solutions to the `n queens' problem.
 
-    They are explored in lexical order.  Value yielded at each step is a
-    Solution object - this is a Permutation with a custom repr() as a picture
-    (and a method, peaceful, which shall return True).\n"""
+        They are explored in lexical order.  Value yielded at each step is a
+        Solution object - this is a Permutation with a custom repr() as a
+        picture.\n"""
+        for it in cls.all(size):
+            if it.__peaceful(): yield it
 
-    for it in permute.Iterator(size, Solution):
-        if it.peaceful(): yield it
+    @classmethod
+    def unique(cls, size):
+        """Like all, q.v., but skips essentially equivalent solutions."""
+        a = ()
+        for r in cls.solve(size):
+            if r not in a:
+                yield r
+                a = a + cls.__equivalents(r, size)
 
-    raise StopIteration
-
-def Unique(size=8):
-    """Like Iterator, q.v., but skips essentially equivalent solutions."""
-    def entwist(seq, r, n):
+    def entwist(seq, r, n): # tool used by __equivalents
         seq.append(r)
-	r = map(lambda i: n-i, r) # top-bottom reflection
-	if r not in seq: seq.append(r[:]) # copy before reversing !
-	r.reverse() # left-right reflection
-	if r not in seq:
-	    seq.append(r)
-	    r = map(lambda i: n-i, r) # top-bottom reflection
-	    if r not in seq: seq.append(r)
+        r = map(lambda i: n-i, r) # top-bottom reflection
+        if r not in seq: seq.append(r[:]) # copy before reversing !
+        r.reverse() # left-right reflection
+        if r not in seq:
+            seq.append(r)
+            r = map(lambda i: n-i, r) # top-bottom reflection
+            if r not in seq: seq.append(r)
             # r.reverse() would restore its initial value
 
-    def equivalents(r, n, add=entwist):
+    @staticmethod
+    def __equivalents(r, n, add=entwist):
         row = []
         add(row, r, n)
         r = r.inverse # diagonal reflection
@@ -73,10 +80,9 @@ def Unique(size=8):
             add(row, r, n)
         return tuple(row)
 
-    a = ()
-    for r in Iterator(size):
-	if r not in a:
-	    yield r
-	    a = a + equivalents(r, size)
+    del entwist
 
-    raise StopIteration
+del Permutation
+# backwards compatibility:
+def Iterator(size=8): return Solution.solve(size)
+def Unique(size=8): return Solution.unique(size)
