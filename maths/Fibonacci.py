cd389106e2fb707d31f82b883a26fa22aa6f3c75
Edward Welbourne <eddy@chaos.org.uk>
2012-07-15 15:55:30 +0200

    Added Fibpair and refined existing parts.
    Fibpair implements the underlying modular polynomials used by fastonacci
    and provides more flexibility.  Added iterator support and partial "is
    in" support for the cached Fibonacci sequences (and renamed a local
    variable).  Fixed an out-of-date reference to BigFloat.  Reworked fibpow
    to cope with negative n and take the right arguments to make fastonacci
    simple.

--- maths/Fibonacci.py
+++ maths/Fibonacci.py
@@ -30,8 +30,9 @@ iteration, adding an external square brings the ratio of sides closer to the
 golden ratio (by a factor of about 2.6).  If we start with unit sides we are
 effectively computing the Fibonacci sequence, so we may infer that the ratio
 between successive terms of this sequence steadilly approaches the golden ratio.
-"""
 
+For related discussion, see http://www.chaos.org.uk/~eddy/craft/Fibonacci.html
+"""
 
 def fibonacci(what, was=0, result=1):
     """Computes Fibonacci's sequence.
@@ -53,7 +54,7 @@ def fibonacci(what, was=0, result=1):
     overflow by coercing values to indefinite-precision integers (Python's long
     integer type).  If actual initial values are floats, this handling of
     overflow is probably wrong; you may be better off using
-    basEddy.bigfloat.BigFloat()s.\n"""
+    study.value.bigfloat.BigFloat()s.\n"""
 
     if -1 < what < 1: return was
     while what > 1:
@@ -90,8 +91,10 @@ def fibtimes((a,b), (c,d)):
 
     return a * (c + d) + b * c, a * c + b * d
 
-def fibpow(n, a=1, b=0):
-    c, d = 0, 1 # the identity
+def fibpow(n, c=0, d=1):
+    """Returns (c, d) * (1, 0)**n"""
+    if n < 0: a, b, n = 1, -1, -n
+    else: a, b = 1, 0
     while n > 0:
 	n, i = divmod(n, 2)
 	if i: c, d = fibtimes((a,b),(c,d))
@@ -99,35 +102,173 @@ def fibpow(n, a=1, b=0):
     return c, d
 
 def fastonacci(n, zero, one):
-    """As for fibonacci, but computed in logarithmic time :-)"""
+    """As for fibonacci, but computed in logarithmic time :-)
 
-    return fibtimes((zero, one), fibpow(n))[0]
+    This (together with the supporting functions above) is essentially just
+    the bare necessities condensed out of Fibpair.fibonacci, below; but
+    Fibpair provides greater flexibility, e.g. see its .beyond().\n"""
+
+    return fibpow(n, zero, one)[0]
 
-# or ...
+class Fibpair (tuple):
+    """Pairs representing polynomials modulo lambda x: x*x -x -1
+
+    This approach (or fastonacci, which is equivalent) is best when working
+    with sparse entries, some at large index, in Fibonacci's sequence.  If
+    working with a contiguous block, it is typically better to use a cached
+    approach, although the pair whose first entry .fibonacci() returns can be
+    used to seed that.  See beyond() for an iterator like this.
+
+    See http://www.chaos.org.uk/~eddy/craft/Fibonacci.html#Poly for details.\n"""
+    __upnew = tuple.__new__
+    # automagically @classmethod:
+    def __new__(cls, a, b): return cls.__upnew(cls, (a, b))
+    # def __init__(self, a, b): pass # over-ride tuple.__init__'s different signature.
+
+    # Support for arithmetic:
+    @staticmethod
+    def __unpack(other, what):
+        try: c, d = other
+        except TypeError:
+            try: 0 + other
+            except:
+                raise ValueError(
+                    'Fibpair can %s another Fibpair or a simple scalar' % what, other)
+            c, d = 0, other
+        return c, d
+
+    @classmethod
+    def __add(cls, (a, b), (c, d)): return cls(a + c, b + d)
+    def __add__(self, other):
+        return self.__add(self, self.__unpack(other, "add"))
+    def __radd__(self, other):
+        return self.__add(self.__unpack(other, "be added to"), self)
+
+    @classmethod
+    def __sub(cls, (a, b), (c, d)): return cls(a - c, b - d)
+    def __sub__(self, other):
+        return self.__sub(self, self.__unpack(other, "subtract"))
+    def __rsub__(self, other):
+        return self.__sub(self.__unpack(other, "be subtracted from"), self)
+
+    @classmethod
+    def __mul(cls, (a, b), (c, d)):
+        return cls(a * (c + d) + b * c, a * c + b * d)
+    def __mul__(self, other):
+        return self.__mul(self, self.__unpack(other, "multiply by"))
+    def __rmul__(self, other):
+        return self.__mul(self.__unpack(other, "multiply"), self)
+
+    @classmethod
+    def __div(cls, (a, b), (c, d)):
+        """Division.
+
+        This solves: (a, b) = (c, d) * (x, y), i.e.
+           a, b = c * (x + y) + d * x, c * x + d * y
+        The equation for b re-arranges to:
+           y = (b - c * x) / d
+        Then that for a becomes:
+           a * d - c * b = (c * d - c * c + d * d) * x
+        whence
+           x = (a * d - c * b) / (c * d - c * c + d * d)
+        so:
+           y = (b * (c * d -c * c +d * d) -c * (a * d -c * b)) / d / (c * d -c * c +d * d)
+             = (b * c + b * d - a * c) / (c * d - c * c + d * d)
+        Then (a, b) = (c, d) * (x, y), so (x, y) is (a, b) / (c, d).\n"""
+
+        den = (c + d) * d - c * c
+        x, y = a * d - c * b, b * (c + d) - a * c
+        if den * (x / den) == x: x /= den
+        else: x *= 1. / den
+        if den * (y / den) == y: y /= den
+        else: y *= 1. / den
+        return cls(x, y)
+
+    def __div__(self, other):
+        return self.__div(self, self.__unpack(other, "divide by"))
+    def __rdiv__(self, other):
+        return self.__div(self.__unpack(other, "divide"), self)
+
+    @classmethod
+    def __unit(cls): return cls(0, 1)
+    def __pow__(self, n):
+        if n < 0: x, n = self.__div((0, 1), self), -n
+        else: x = self
+
+        n, i = divmod(n, 2)
+        if i: ans = x
+        else: ans = self.__unit()
+
+        while n > 0:
+            x *= x
+            n, i = divmod(n, 2)
+            if i: ans *= x
+        return ans
+
+    # Make it useful:
+
+    @classmethod
+    def beyond(cls, n=0, zero=0, one=1):
+        """Returns an iterator over the Fibonacci sequence.
+
+        All arguments are optional:
+
+        This takes the given f(0) and f(1) and iterates i from n away from
+        zero, yielding each f(i).  If n is 0, you'll get f(0), f(1), ...; if n
+        is -1, you'll get f(-1), f(-2), ...; any other n simply skips the
+        early entries in one of these.\n"""
+
+        x = cls(1, 0)
+        here = cls(zero, one) * x**n
+        if n < 0: x = cls(1, -1)
+        while True:
+            yield here[0]
+            here *= x
+
+    @classmethod
+    def fibonacci(cls, n, zero=0, one=1):
+        return (cls(zero, one) * cls(1, 0)**n)[0]
+
 class Fibonacci:
+    """Cached computation of Fibonacci's sequence."""
     def __init__(self, zero=0, one=1):
-	self.__natural, self.__negative = [ zero, one ], []
+	self.__natural, self.__negative = [ zero, one ], [ one - zero, 2 * zero - one ]
+
+    def __iter__(self):
+        i = 0
+        while True:
+            yield self[i]
+            i += 1
 
     def __up(self, n):
 	assert n >= 0
 	row = self.__natural
-	left = n + 1 - len(row)
-	while left > 0:
+	todo = n + 1 - len(row)
+	while todo > 0:
 	    row.append(row[-1] + row[-2])
-	    left = left - 1
+	    todo -= 1
 
-	return self.__natural[n]
+	return row[n]
 
     def __down(self, n):
 	assert n > 0
 	row = self.__negative
-	left = n - len(row)
-	while left > 0:
+	todo = n - len(row)
+	while todo > 0:
 	    row.append(row[-2] - row[-1])
-	    left = left - 1
+	    todo -= 1
 
 	return row[n-1]
 
     def __getitem__(self, n):
 	if n < 0: return self.__down(-n)
 	return self.__up(n)
+
+    def __contains__(self, other, g=.5*(1 +5.**.5)):
+        if self.__natural[:2] == [ 0, 1 ] and other > 0:
+            # http://en.wikipedia.org/wiki/Fibonacci_number#Recognizing_Fibonacci_numbers
+            lo, hi = g * other, 1./other
+            lo, hi = lo - hi, lo + hi
+            return lo <= int(hi) > 0
+
+        raise NotImplementedError
