a95ec3cdc6fdd004399c2bd3300218368872bd99
Edward Welbourne <eddy@chaos.org.uk>
2012-07-28 16:54:28 +0200

    I've moved lazy to snake.  Import quantity and SI by default.

--- value/__init__.py
+++ value/__init__.py
@@ -3,10 +3,11 @@
 Fragments:
   archaea -- archaic and/or silly units
   bigfloat -- extends the range of floating-point values
-  lazy -- helper classes for lazy evaluation
   object -- provides a generic Object class
   quantity -- describes a value with units of measurement
   sample -- describes a number with uncertain value
   SI -- the base units of SI
   units -- other vaguely sensible units
 """
+import quantity
+import SI
