172a37cd36cb3cb3ab96789b599e3ad372e64f97
Edward Welbourne <eddy@chaos.org.uk>
2012-10-13 14:56:57 +0200

    More documentation, options and other refinements for voronoi.Catchment
    Replaced the same=True parameter of .nearest() with an elide=()
    parameter, for greater generality (at no cost in code complexity; and
    it's easier to explain in the doc).  Fixed handling of this (when same
    was True, so that ancestors of the box containing where could be empty
    if their other leaves are, one of these ancestors could cause the actual
    nearest neighbour's box to be eliminated in .__nearleaves()) by setting
    .points on all nodes in the BTree, not just the leaves.
    
    Reworked BTree somewhat.  Splitting now prefers to happen on a less
    recently used index even if a more recently used one has a smaller
    diameter (to avoid bad behaviour if many points lie on some hyperplane
    with one coordinate constant).  Added a .parent attribute in support of
    that.  Details have shifted around a bit among BTree's private methods
    in the process, notably turning its @classmethod .__btree() into a
    .__child() instance method.
    
    Made the internal bisection tree private (s/\.tree/.__root/wg).
    Let client tune the number of points in each BTree.
    Documented the classes and most methods.
    Added page-splits in strategic places.

--- maths/voronoi.py
+++ maths/voronoi.py
@@ -12,52 +12,133 @@ class Catchment (object):
     """Subdivide space according to which of a set of points is nearest.
 
     This is Voronoi's decomposition of the space; see class Voronoi for the
-    description of the regions into which the space is thus decomposed.\n"""
+    description of the regions into which the space is thus decomposed.  Records
+    the result of ingesting its constructor's argument (see .__init__()) as
+    .centres; has one method, .nearest(vec [, elide]), that finds the nearest in
+    .centres (but not in elide, if passed) to the given vec.\n"""
 
-    def __init__(self, centres):
+    def __init__(self, centres, count=1):
         """Set up data for computation of catchment-regions.
 
-        Single argument, centres, should be an iterable, whose entries are
-        points in some vector space; if they are not Vector instances, they
-        should be sequences acceptable to Vector.fromSeq().  The space is
-        divided up into cells, one per point; each point's cell comprises the
-        portion of the space that's closer to this point than any other.\n"""
+        Required first argument, centres, should be an iterable, whose entries
+        are points in some vector space; if they are not Vector instances, they
+        should be sequences acceptable to Vector.fromSeq().  Optional second
+        argument, count, is an upper bound on the number of points to include in
+        each leaf of the bisection tree built internally; it must be at least 1
+        - its default (and recommended) value.
+
+        Stores, as .centres, a tuple of Vector (see study.maths.vector)
+        instances (made using Vector.fromSeq where needed) representing the
+        entries in centres.\n"""
+
+        if count < 1: raise ValueError('Demanding unrealistic subdivision', count)
 
         self.centres = tuple(map(self.__vectorise, centres))
         # Check all in the same space:
         each = iter(self.centres)
-        dims = each.next().dimension
+        dims = each.next().dimension # StopIteration if no centres, which would be silly
         for it in each:
             d = it.dimension
             if len(d) != len(dims) or d != dims:
                 raise ValueError('Mismatched dimension', dims, d, it)
         self.__dim = tuple(dims)
-        self.tree = self.__btree(self.centres)
+        self.__root = self.__btree(self.centres, count)
 
     @staticmethod
     def __vectorise(val, V=Vector):
         if isinstance(val, V): return val
         return V.fromSeq(val)
-
+
+    # Support for .__btree()
     class BTree (object):
-        def __init__(self, lo, hi): self.box = lo, hi
-        @classmethod
-        def __btree(cls, lo, hi): return cls(lo, hi)
+        """Bisection tree.
+
+        A coordinate-aligned cuboid box that knows how to be split in two.  Has,
+        as its .box attribute, a pair of vectors (low, high); the cuboid it
+        describes has .box[0][ind] <= vec[ind] <= .box[1][ind] for each vec in
+        the cuboid and each full-rank index, ind.  Supports 'vec in self'
+        testing based on this.
+
+        If it's been .split(), it has child notes .left and .rite and its .cut
+        records the coordinate at their boundary; otherwise these attributes are
+        unset.  The value of .cut (when set) is a twople whose first entry is
+        the index of the coordinate and whose sencond entry is the value of this
+        coordinate at the boundary between the two child nodes.
+
+        Has .parent set to the node whose .split() created it as .left or .rite,
+        if this is how it was created; otherwise, doesn't have .parent set (so
+        AttributeError is the easy way out of traversal up the .parent
+        hierarchy).  Methods:
+
+          .split() -- bisect self on a suitable coordinate plane.
+          .locate(vec) -- find which cell a given vec is in
+          .distance(vec) -- range of distance between vec and points in self.box
+
+        Our one client, Catchment, shall set .points to a list of points in the
+        box (but no method of this class pays any attention to .points).\n"""
+        def __init__(self, lo, hi, up=None):
+            self.box = lo, hi
+            if up is not None: self.parent = up
+
+        # Tools for use by split():
+        def __child(self, lo, hi): return self.__class__(lo, hi, self)
 
         @staticmethod
-        def __split(ind, lo, hi, only=Vector.delta):
-            gap = hi[ind] - lo[ind]
+        def __split(deep, lo, hi, only=Vector.delta):
+            """Finds the right index and mid-value on which to split a box.
+
+            Uses deep (from __depth(), below) to determine how recently each
+            index has been split on; amongst the least recently used, finds the
+            one for which hi-lo has the largest component, i.e. the box's width
+            is biggest.  Returns a twople of this index with the vector that's
+            half the box's width in this direction.\n"""
+            dex = (hi - lo).iteritems()
+            big, was = dex.next()
+            assert was >= 0
+            at = deep(big)
+
+            for ind, val in dex:
+                assert val >= 0
+                dp = deep(ind)
+                if at < dp or (dp == at and val < was):
+                    at, was, big = dp, val, ind
+
+            gap = hi[big] - lo[big]
             assert gap > 0
             q, r = divmod(gap, 2)
             if r: q = gap * .5 # but prefer an int if we got one
-            return only(hi.dimension, ind) * q
+            return big, only(hi.dimension, big) * q
+
+        def __depth(self):
+            """Returns a function to compute depth below last split on an index.
+
+            The returned function returns one more than self's depth in the tree
+            when we haven't yet split on the index given.\n"""
+            up, prior = self, []
+            try:
+                while True:
+                    up = up.parent # AttributeError at root
+                    assert hasattr(up, 'cut')
+                    prior.append(up.cut[0])
+            except AttributeError: pass
+
+            def deep(ind, p=prior):
+                try: return p.index(ind)
+                except ValueError: return len(p)
+
+            return deep
 
         def split(self):
+            """Split self in half in one coordinate direction.
+
+            The chosen coordinate direction is the least recently used, among
+            splits in the tree down to self, preferring those with bigger width
+            among those on which we haven't split previously.\n"""
+
             lo, hi = self.box
-            ind = (hi - lo).biggest
-            gap = self.__split(ind, lo, hi)
-            assert len(hi) == len(gap), (gap, ind, hi, lo)
-            self.left, self.rite = self.__btree(lo, hi - gap), self.__btree(lo + gap, hi)
+            ind, half = self.__split(self.__depth(), lo, hi)
+            assert len(hi) == len(half), (half, ind, hi, lo)
+            self.left, self.rite = self.__child(lo, hi - half), self.__child(lo + half, hi)
             self.cut = ind, self.left.box[1][ind]
             assert self.cut[1] == self.rite.box[0][ind]
             return self.left, self.rite
@@ -66,18 +147,29 @@ class Catchment (object):
             lo, hi = self.box
             return not vec.pointwise(lambda v, L, H: v < L or H < v, None, lo, hi)
 
-        def locate(self, vec):
+        def locate(self, vec): # not actually used
+            """Returns the leaf node containing a given vector.
+
+            Identifies the leaf node, in the tree under self, in which vec
+            appears; or the one closest to vec, if vec is not in self.\n"""
+
             here = self
             while True:
                 try: ind, cut = here.cut
                 except AttributeError: break
                 here = here.rite if vec[ind] > cut else here.left
-                # equivalent to, but cheaper than:
+                # For vec in self, this is equivalent to, but cheaper than:
                 # here = left if vec in left else rite
-            assert vec in here
+
+            assert vec in here or not vec in self
             return here
 
         def distance(self, vec):
+            """Returns the range of distances, in self's box, from a given vector.
+
+            Required argument, vec, is the vector to measure from.  Returns a
+            twople of the distances from vec to its nearest and furthest points
+            in self.\n"""
             lo, hi = self.box
             off = vec.pointwise(lambda v, L, H: L - v if v <= L else H -v if v >= H else 0,
                                  None, lo, hi)
@@ -89,88 +181,116 @@ class Catchment (object):
     def __btree(ps, each=1, Tree=BTree):
         """Builds a BTree with at most each of the ps in each leaf of the tree.
 
-        Saves, as each leaf's .points, the list of entries from ps that are in
-        the leaf.  Points on boundaries are placed in one of the adjacent
-        leaves; clients should make no assumption about which.\n"""
+        Saves, as each node's .points, the list of entries from ps that are in
+        the leaf.  Points on boundaries are placed in one of the adjacent nodes,
+        at each split; clients should make no assumption about which.\n"""
         root = Tree(ps[0].pointwise(lambda *xs: min(xs), None, *ps[1:]),
                     ps[0].pointwise(lambda *xs: max(xs), None, *ps[1:]))
 
         live = [ ( root, ps ) ]
         while live:
             tree, ps = live.pop()
-            if len(ps) > each:
+            assert all(p in tree for p in ps)
+            tree.points = tuple(ps)
+
+            if len(ps) > each: # else we can let tree be a leaf
                 left, rite = tree.split()
                 ind, cut = tree.cut
                 ls, rs = [], []
                 for p in ps: (rs if p[ind] > cut else ls).append(p)
                 live += [ (left, tuple(ls)), (rite, tuple(rs)) ]
-            else: # sub-divided enough; record which points are in this leaf:
-                assert all(p in tree for p in ps)
-                tree.points = tuple(ps)
 
         return root
     del BTree
-
+
+    # Support for .nearest()
     from itertools import chain
     @staticmethod
-    def __nearleaves(where, tree, same, byturns=chain):
+    def __nearleaves(where, tree, elide, byturns=chain):
+        """Find the leaf nodes in which the closest point to where could lie.
+
+        Arguments:
+          where -- the point;
+          tree -- the tree to traverse;
+          elide -- collection of points to ignore, in .points attributes.
+
+        Expands each split in tree so as to discard (without splitting) every
+        box whose nearest point to where is further than the furthest point of
+        some other box; repeatedly splits the box, whose furthest point from
+        where is nearer than that of any other, so as to be able to discard as
+        many futher boxes as soon as possible.  Each box retained, at the end,
+        is a leaf with at least some point in it closer to where than the
+        furthest point of every retained box.  The return is an iterator over
+        the points in the boxes retained; these are the points that might be
+        nearest to where.\n"""
+
         near, best = tree.distance(where)
         live, more = [ ( tree, near ) ], False
+        if elide: empty = lambda ps, ig=elide: not [ p for p in ps if p not in ig ]
+        else: empty = lambda ps: not ps
+        if empty(tree.points):
+            raise ValueError('Ignoring all points leaves none to be nearest', elide)
 
         # Invariants: best is the max distance from where of any point in tree's
         # box; tree is the live BTree node for which this is least, among those
         # we've seen; more is true when we've split a node since last our
         # scanning showed us tree.  We loop over the live BTree nodes, splitting
         # those we can and eliminating those in which the nearest point can't
-        # be.
-
+        # be.  There are no empty() entries in live.
         while True:
             here, near = live.pop()
             if near > best: continue # we can ignore this sub-tree
 
             try: pair = here.left, here.rite
             except AttributeError:
-                if here.points and (same or here.points != (where,)):
-                    live.insert(0, (here, near)) # put it back
-                if here is tree:
+                live.insert(0, (here, near)) # Move to the back of the queue
+                if here is tree: # Are we done yet ?
                     if more: more = False
-                    else: break # we're looping
-            else:
-                more = True
+                    else: break # no further refinement possible
+
+            else: # split node
+                more, shift = True, here is tree
                 for here in pair:
-                    try:  # if it's a leaf with no points, ignore it:
-                        if not here.points: continue
-                    except AttributeError: pass # not a leaf
+                    # if it's a leaf with no points, ignore it:
+                    if empty(here.points): continue
+
                     near, far = here.distance(where)
-                    if near < best:
-                        if best < far:
-                            # Put it at the back of the queue
-                            live.insert(0, (here, near))
+                    if near < best: # else ignore here
+                        # When splitting tree itself (shift is true), one of its
+                        # children must be at least as good as it and we must
+                        # switch to using that as tree, so that the "Are we done
+                        # yet ?" does get triggered.  If splitting some other
+                        # node equals tree, but doesn't beat it, then we'll
+                        # still be done when we get back to tree in the loop, so
+                        # no need to change which node we think is best.
+                        if (best <= far if shift else best < far):
+                            live.insert(0, (here, near)) # Back of the queue.
                         else:
                             best, tree = far, here
                             # Reprocess promptly, as splitting it is apt to improve best:
                             live.append((here, near))
+
         assert live
         # Remaining leaves overlap, need to check all points in them.
         return byturns(*tuple(it[0].points for it in live))
     del chain
 
-    def nearest(self, where, same=True):
+    def nearest(self, where, elide=()):
         """Find a nearest entry in self.centres to where.
 
-        Required first argument, where, is a vector (or tensor, of the same kind
-        as self.centres).  Optional second argument, same, indicates whether
-        where itself can be used as nearest point, if it happens to be one of
-        self's centres; it defaults to True - set it to False to find the
-        nearest non-equal point.  Returns a twople (p, d) where p is in
-        self.centres and d*d == (where-p).squaresum is minimal among p in
-        self.centres (although there is no guarantee this is unique).\n"""
+        Required first argument, where, is a vector (or tensor, with the same
+        .dimension as the entries in self.centres).  Optional second argument,
+        elide, is a container (we must be able to do 'p in elide' tests) of
+        points in self to ignore when searching for the one nearest to where;
+        defaults to ().  Returns a twople (p, d) where p is in self.centres and
+        d*d == (where-p).squaresum is minimal among relevant p (in self.centres
+        but not in elide) - although there is no guarantee this is unique.\n"""
 
-        ps = self.__nearleaves(self.__vectorise(where), self.tree, same)
+        ps = self.__nearleaves(self.__vectorise(where), self.__root, elide)
         centre = ps.next()
         sumsqr = (centre - where).squaresum
         for p in ps:
-            if not same and p == where: continue
+            if p in elide: continue
             rr = (p - where).squaresum
             if rr < sumsqr: centre, sumsqr = p, rr
 
