bc98f251d7694b764e86f9a41703f1c9de7b2fb9
Edward Welbourne <eddy@chaos.org.uk>
2014-09-06 21:39:38 +0200

    Enhance Module to package imports within modules it loads.
    Previously, when the source file for a directory did an import of other python
    modules within the directory, or of a sub-directory, they'd be represented by
    ordinary module objects, without auto-loading functionality.  So rework the
    loading to massage any module objects loaded into a namespace to be represented
    via the object our auto-loading would have loaded in its place !  This is all
    just one huge kludge-around for now knowing a way to offer the import machinery
    a callable of my chosing that it can use in place of the builtin module().
    
    Also gave the root object a smarter default name, tweaked the repr and docs.

--- cache/module.py
+++ cache/module.py
@@ -31,13 +31,32 @@ class Module (modbase):
     Root().\n"""
 
     @classmethod
-    def Root(cls, directory, name='root', src='__init__.py'):
+    def Root(cls, directory, name=None, src='__init__.py'):
         """Instantiate the root of a lazily-loaded module hierarchy.
 
-        Attribute lookup on the returned object shal serve as 
-        """
-        inst = cls(name, directory, src)
-        return cls.__load_file(inst.__path__, inst, directory)
+        Required argument, directory, is the root directory of the
+        hierarchy.  Next positional argument, name, is optional; if given (and
+        not None), it is used as the __name__ of the module object.  Third
+        positional argument, src, is the name of a file (typically with a '.py'
+        extension) to serve as the module loaded from each directory to
+        represent its 'package' object within that directory; the default is
+        '__init__.py'.  (This name may even include path separators.)  The
+        default for name obtained by stripping the extension (if any) from the
+        last component, that doesn't start with '__', of the result of
+        path-joining directory to src.  If all path components considred by that
+        start with '__' then the name 'root' is used.
+
+        Attribute lookup on the returned module object shall autoload submodules
+        and subpackages (using the same src file in each) whenever attribute
+        lookup would otherwise fail and a relevant file exists.\n"""
+
+        if name is None:
+            name = cls.__base_name(src)
+            if name is None:
+                name = cls.__base_name(directory)
+                if name is None: name = 'root'
+
+        return cls(name, directory, src).__load_self()
 
     __updelat = modbase.__delattr__
     def __delattr__(self, key):
@@ -45,6 +64,17 @@ class Module (modbase):
         except KeyError: pass
 
         return self.__updelat(key)
+
+    __upsetat = modbase.__setattr__
+    def __setattr__(self, key, value):
+        if isinstance(value, Module) and value.__name__ == self.__name__ + '.' + key:
+            # This is exactly what a lazy-lookup would have given us: clear any
+            # prior attribute for key (maybe in __dict__) and move to __cache.
+            try: delattr(self, key)
+            except AttributeError: pass
+            self.__cache[key] = value
+        else:
+            self.__upsetat(key, value)
 
     def __getattr__(self, key):
         try: return self.__cache[key]
@@ -60,25 +90,14 @@ class Module (modbase):
                              key, self.__name__)
 
     def __repr__(self):
-        return """<lazily-loaded Module %s, from file %s in %s>""" % (
-            self.__name__, self.__src, self.__dir)
+        return """<%s '%s', lazily loaded from '%s' in '%s'>""" % (
+            self.__class__.__name__, self.__name__, self.__src, self.__dir)
 
-    @classmethod
-    def __sub_mod(cls, name, directory, src):
-        return cls(name, directory, src)
-
-    @staticmethod
-    def __sub_raw(name, path, base=modbase):
-        inst = base(name, """Lazily-loaded sub-module %s
-
-Loaded from file %s
-""" % (name, path))
-        inst.__path__ = path
-        return inst
-
+    # Only implementation internals below.
     import sys
     @staticmethod
-    def __load_file(path, into, indir, modmap=sys.modules, searchpath=sys.path):
+    def __load_file(path, into, indir, repack,
+                    modmap=sys.modules, searchpath=sys.path):
         """Load python code into the namespace of an object.
 
         The object should be based on the builtin module type. It is returned,
@@ -91,35 +110,184 @@ Loaded from file %s
         modmap[name] = into
         searchpath.insert(0, indir)
 
+        # TODO: can we replace what the system uses as <type module> when doing
+        # its imports ?  See .__repack()'s documentation.
+
         try:
             fd = open(path)
             try: exec fd in into.__dict__
             finally: fd.close()
         finally: # Restore module protocol status quo:
             try: ind = searchpath.index(indir)
-            except ValueError: pass
-            else: del searchpath[:ind+1]
+            except ValueError: print 'Someone already pruned sys.path for me :-!\n', path
+            else: del searchpath[:ind+1] # also pop anything added during loading.
 
             if restore: modmap[name] = prior
+            # Should we, in fact, leave this in place ?
+            # It might mitigate the issue __repack() works round.
             else: del modmap[name]
 
-        return into
+        return repack(into)
     del sys
 
+    def __load_self(self):
+        try: return self.__load_file(self.__path__, self, self.__dir, self.__repack)
+        except IOError: return self # Directory with nothing specific to load
+
+    def __load_raw(self, name, path, base=modbase):
+        inst = base(name, """%s '%s'
+
+Lazily loaded from file %s by '%s' '%s'.
+""" % (base.__name__, name, path, self.__class__.__name__, self.__name__))
+        inst.__path__ = path
+        return self.__load_file(path, inst, self.__dir, self.__repack)
+
+    def __repack(self, mod, base=modbase):
+        """Repackage sub-modules imported by a sub-module.
+
+        Looks at mod's namespace, skipping any '__'-started names, for module
+        objects that it would make sense to replace with Module objects.  For
+        now, only sub-modules of self are considered; it may make sense to also
+        apply this to sub-modules of other Module objects.
+
+        If we could replace the builtin module(), a method of self - using
+        self.__sub_{raw,mod} when appropriate, and maybe even the matching
+        methods of other things in self's tree when suitable, falling back on
+        the builtin module() otherwise - would let us handle imports in the
+        loaded text gracefully.  At present, we just get raw module objects when
+        the loading does an import, breaking the lazy loading via those.  Which
+        is why we need repack.  That and the fact that __load_file() saved to
+        mod.__dict__ even what belongs in mod.__cache.\n"""
+
+        assert isinstance(mod, base)
+        assert mod is self or not isinstance(mod, Module)
+
+        try: repacking = not mod.__repacked
+        except AttributeError: mod.__repacked = False
+        else:
+            if repacking:
+                print 'Wimping out of recursive repack', mod.__name__, mod.__file__
+                # When we re-wind from the recursion, all should be well anyway.
+            # else: nothing to do, job has already been done.
+            return mod
+
+        ismod = isinstance(mod, Module)
+        for k in dir(mod):
+            if k.startswith('__'): continue
+
+            submod = getattr(mod, k)
+            if isinstance(submod, Module):
+                if ismod and mod.__name__ + '.' + k == submod.__name__:
+                    # __load_file stored to mod.__dict__; move to mod.__cache:
+                    setattr(mod, k, submod)
+                # In any case, submod's own loading did any needed repacking.
+                assert submod.__repacked
+
+            elif isinstance(submod, base):
+                submod = self.__as_sub_mod(submod)
+                # That's None if replacement doesn't make sense or no suitable
+                # replacement is available.  Otherwise, one way or another, it
+                # came from __load_sub(), so submod has been subjected to any
+                # needed repack()ing.
+                if submod is not None:
+                    # Over-write the raw module attribute with its replacement:
+                    setattr(mod, k, submod)
+                    assert submod.__repacked
+
+        mod.__repacked = True
+        return mod
+
     import os
+    @staticmethod
+    def __base_name(name, split=os.path.split, prune=os.path.splitext):
+        """Implements Root()'s default for name."""
+        path, name = split(name)
+        while name.startswith('__'):
+            if path: path, name = split(path)
+            else: return None
+
+        return prune(name)[0]
+
+    @staticmethod
+    def __in_dir(stem, name,
+                 abspath=os.path.abspath,
+                 seps=tuple(x for x in (os.path.sep, os.path.altsep) if x)):
+        if not name.startswith(stem):
+            stem = abspath(stem)
+            if not name.startswith(stem):
+                name = abspath(name)
+                if not name.startswith(stem): return None
+
+        if name[len(stem)] in seps: return name[len(stem) + 1:]
+        return None
+
+    @staticmethod
+    def __get_source(mod):
+        # Some modules have .__file__, others have .__path__ !
+        try: path = mod.__file__
+        except AttributeError: path = mod.__path__
+        # A .pyc file's source is the .py from which it's compiled:
+        if path.endswith('.pyc'): path = path[:-1]
+        return path
+
+    def __as_sub_mod(self, other,
+                     split=os.path.split, prune=os.path.splitext):
+        """Suggest a suitable replacement for a raw module object.
+
+        Single parameter, other, is an attribute value of a module object
+        (either raw or lazy-loading) being lazy-loaded; if it's a raw module
+        that could be represented as a lazy-loaded submodule of self, return
+        that replacement; otherwise, return None to keep the original.\n"""
+
+        try: name = self.__get_source(other)
+        except AttributeError: return None
+
+        # TODO: .__in_subdir(node.__dir, name) for node in self's tree
+        relpath = self.__in_dir(self.__dir, name)
+        if not relpath: return None
+        # OK, it is (or looks like) a sub-module:
+
+        path, name = split(relpath)
+        seq = [] if name == self.__src else [ prune(name)[0] ]
+        while path:
+            path, name = split(path)
+            seq.insert(0, name)
+
+        seq = iter(seq)
+        try: nom = seq.next()
+        except StopIteration: return None # Don't believe an apparent self-reference
+
+        # Must do the __load_sub() directly, not via getattr(), for the first as
+        # other may have come as an attribute of self, in which case getattr is
+        # just going to give us other here.  This also ensures we repack submod,
+        # so that any module objects within *it* are taken care of.
+        try: submod = self.__load_sub(nom)
+        except IOError: return None # False positive ?
+        # But now we can rely on getattr(), as *its* calls to __load_sub() shall
+        # do a suitable __repack() where needed.
+        try:
+            for nom in seq: submod = getattr(submod, nom)
+        except AttributeError: return None # False positive ?
+
+        newpath = self.__get_source(submod)
+        if self.__in_dir(self.__dir, newpath) != relpath:
+            print 'Wimping out of replacing', other, 'with', submod
+            return None
+
+        return submod
+
     def __load_sub(self, key,
                    join=os.path.join,
                    exists=os.path.exists,
                    isdir=os.path.isdir):
         name = self.__name__ + '.' + key
-        path = join(self.__dir, key + '.py')
+        path = join(self.__dir, key + '.py') # TODO: also check for .pyc ?
         if exists(path):
-            return self.__load_file(path, self.__sub_raw(name, path), self.__dir)
+            return self.__load_raw(name, path)
 
         path = path[:-3] # sub-directory: drop the .py extension
         if isdir(path):
-            inst = self.__sub_mod(name, path, self.__src)
-            return self.__load_file(inst.__path__, inst, path)
+            return self.__class__(name, path, self.__src).__load_self()
 
         raise IOError
 
@@ -128,11 +296,11 @@ Loaded from file %s
         """Internal constructor; use Root() to construct your root object."""
         self.__path__ = join(directory, src)
         self.__upinit(name, # and a doc-string:
-                      """Lazily-loaded Module %s
+                      """%s '%s'
 
-Loaded from file %s
-in %s
-""" % (name, src, directory))
+Loaded lazily from '%s'
+in '%s'
+""" % (self.__class__.__name__, name, src, directory))
         self.__dir, self.__src = directory, src
         self.__cache = {}
 
