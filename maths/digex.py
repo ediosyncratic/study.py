6fcc2250f2dd6e8e4eda17fb55af188bf1600e99
Edward Welbourne <eddy@chaos.org.uk>
2025-03-31 07:07:06 +0200

    Improved Digex somewhat.
    Fixed some dumb bugs, also handling of negative inputs (which could
    previously lead to -1 digits, due to float imprecision).

--- maths/digex.py
+++ maths/digex.py
@@ -8,12 +8,6 @@ See study.LICENSE for copyright and license information.
 """
 import math
 
-def _fracSplit(v):
-    u = int(v)
-    if u > v: u += 1
-    assert 0 <= v -u < 1
-    return u, v -u
-
 class Digex (object):
     """Generator for digits of an exponential.
 
@@ -28,7 +22,7 @@ class Digex (object):
 
     We can now start with the highest-index s[i] that's >= i > 1,
     divide it by i, leave the remainder as s[i] and add the quotient
-    into s[i-1]; working doown through decreasing i, we then get our
+    into s[i-1]; working down through decreasing i, we then get our
     sequence into a form where, aside from s[1], each s[i] < i.
 
     Once we have this initial representation of our sum, s[1] is the
@@ -38,15 +32,15 @@ class Digex (object):
     the fractional part separator before proceeding further.
 
     Once we've dealt with the whole-number part of 10**n times our
-    value, and cleared s[1], we have a sum with each s[i] < i and 1 >
-    (1/2).(s[2] +(1/3).(s[3] +(1/4).(s[4] +...))).  If we now scale
-    each entry in s by 10 and repeat the sweep from highest index s[i]
-    >= i > 1, subsequently decreasing i to 1, as we q, s[i] =
-    divmod(s[i], i); s[i-1] += q, the resulting s[1] shall be < 10
-    (because the sum, before we scaled it by 10, was < 1) and the
-    whole number part of 10**(n+1) times our value, i.e. its next
-    digit.  We then emit s[1], clear it and repeat this operation to
-    get each successive digit.
+    value (which the preceding has done for n = 0), and cleared s[1],
+    we have a sum with each s[i] < i and 1 > (1/2).(s[2] +(1/3).(s[3]
+    +(1/4).(s[4] +...))).  If we now scale each entry in s by 10 and
+    repeat the sweep from highest index s[i] >= i > 1, subsequently
+    decreasing i to 1, as we (q, s[i]) = divmod(s[i], i); s[i-1] += q,
+    the resulting s[1] shall be < 10 (because the sum, before we
+    scaled it by 10, was < 1) and the whole number part of 10**(n+1)
+    times our value, i.e. its next digit.  We then emit s[1], clear it
+    and repeat this operation to get each successive digit.
 
     Inevitably we have only a finite sub-sum, truncating the power
     series at some index.  We can use logarithms and Stirling's
@@ -72,27 +66,28 @@ class Digex (object):
     set-up object rides further out along s, filling in entries and
     keeping track of the residual error term.  Once that leads to it
     hitting the end of s, it replaces itself with the error-term
-    tracker that trims entries instead.
+    tracker that'll trim entries thereafter instead.
 
     The crucial fact this all relies on is that s[i] in i for all i
-    ensures sum(: s[i]/i! &larr;i :) < 1.  To see this, observe
+    ensures sum(: s[i]/i! &larr;i :) < 1.  To see this, observe that,
+    for any natural n,
 
-      sum(: (i -1)/i! &larr;i; i>1 :)
-        = sum(: 1/(i-1)! -1/i! &larr;i; i>1 :)
-        = sum(: 1/j! &larr;j; j>0 :) -sum(: 1/i! &larr;i; i>1 :)
-        = 1,
+      sum(: (i -1)/i! &larr;i; i>n+1 :)
+        = sum(: 1/(i-1)! -1/i! &larr;i; i>n+1 :)
+        = sum(: 1/j! &larr;j; j>n :) -sum(: 1/i! &larr;i; i>n+1 :)
+        = 1/(n+1)!
 
-    since every term but j = 1 is the same between the two sums.  This
-    is the maximal value that sum(: s[i]/i! &larr;i :) can have, as
-    long as 0 <= s[i] < i for each entry in s.
+    since every term but j = n+1 is the same between the two sums.  In
+    particular, the n = 0 case of this is 1, and is the maximal value
+    that sum(: s[i]/i! &larr;i :) can have, as long as 0 <= s[i] < i
+    for each entry in s.
 
     Note, of course, that this computation is only as accurate as the
     input it's given.  If you ask it for exp(x) for some
     floating-point number x (such as math.log(2), to take an example
     I've tested), it's only accurate to about one part in 2**53 or
-    about 17 decimal places, so the value of exp(x) that gets reported
-    be suject to the same level of imprecision.
-    """
+    about 16 decimal places, so the value of exp(x) that gets reported
+    is suject to the same level of imprecision.\n"""
     class __Stepper (object):
         """Virtual base for __Filler and __Trimmer.
 
@@ -108,45 +103,68 @@ class Digex (object):
         A call to step() may change index.  Its caller should add
         step()'s return to s[index-1] and perform the divmod(s[i], i)
         iteration from there downwards to i = 1 that generates the
-        next digit.
-        """
+        next digit.\n"""
+
+        @staticmethod
+        def fracSplit(v):
+            """Decompose v as an integer and a positive fractional part."""
+            u = int(v)
+            if u > v: u -= 1
+            assert 0 <= v -u < 1
+            return u, v -u
 
     class __Filler (__Stepper):
         carry = 0
         def __init__(self, s, x, base):
             self.x, self.base, self.ticks = x, base, 0
-            i = 1
-            if isinstance(x, int) or isinstance(x, long):
-                s[i] = 1 +x
-                while i +1 < len(s) and s[i] +1 >= i:
-                    i += 1
-                    s[i] = x**i
-                r = 0
+            self.index, self.error = 1, 1 +x
+            if isinstance(x, (int, long)):
+                self.__split = self.intSplit
             else:
-                s[i], r = _fracSplit(1 +x)
-                while i +1 < len(s) and s[i] +1 >= i:
-                    i += 1
-                    s[i], r = _fracSplit(x**i +i * r)
-            self.error = r
-            self.index = i +1
+                self.__split = self.fracSplit
+            self.__fill(s, 1)
+
+        @staticmethod
+        def intSplit(n):
+            return n, 0
+
+        def __fill(self, s, scale):
+            i, x, split = self.index, self.x, self.__split
+            q, r = split(self.error)
+            while i < len(s):
+                if q < 0: r += q
+                else: s[i] = q
+                i += 1
+                p = x**i
+                r = p * scale +i * r
+                if p > 0:
+                    # Coax r positive, if possible, before we go further:
+                    while r < 0:
+                        n, j = divmod(split(r)[0], i)[0], i -1
+                        while j > 0 and not s[j]:
+                            assert n
+                            n, j = divmod(n, j)[0], j -1
+                        assert n
+                        if j > 0:
+                            assert s[j] >= 0
+                            n = max(n, -s[j])
+                            s[j] += n
+                            while j < i:
+                                j += 1
+                                n *= j
+                            r -= n
+                        else:
+                            break
+                q, r = split(r)
+                if abs(q) +1 < i:
+                    break
+            self.error = q +r
+            self.index = i
 
         def step(self, s):
             self.ticks += 1
-            scale = self.base ** self.ticks
-            i, x = self.index, self.x
-            if isinstance(x, int) or isinstance(x, long):
-                assert self.error == 0
-                s[i] = x**i * scale
-                while i +1 < len(s) and s[i] +1 >= i:
-                    i += 1
-                    s[i] = x**i * scale
-            else:
-                s[i], r = _fracSplit((x**i +i * self.error) * scale)
-                while i +1 < len(s) and s[i] +1 >= i:
-                    i += 1
-                    s[i], r = _fracSplit((x**i +i * r) * scale)
-                self.error = r
-            self.index = i +1
+            self.error *= self.base
+            self.__fill(s, self.base ** self.ticks)
 
     class __Trimmer (__Stepper):
         def __init__(self, s, filler):
@@ -154,35 +172,58 @@ class Digex (object):
             assert i == len(s)
             assert i > abs(filler.x) # Computation will be lousy otherwise
             self.index, self.base = i, filler.base
-            r = filler.error * i
+            r = filler.error
             # Our s[i] is somewhere between abs(x)**i and the result
             # of dividing that by (1 -abs(x)/i); both get to be scaled
             # by base**ticks and we add r to each.
             tail = abs(filler.x) ** i * filler.base ** filler.ticks
-            self.__high = tail / (1 -abs(filler.x) / i) +r
+            self.__high = tail / (1 -abs(filler.x) * 1. / i) +r
             self.__tail = tail +r
             self.__trim(s)
 
         def __trim(self, s):
             tail, high, i = self.__tail, self.__high, self.index
-            self.carry = _fracSplit(tail)[0] // i
-            # If high would imply carrying more, wind left until it doesn't:
-            while i > 2 and high > (self.carry +1) * i -1:
+            self.carry = self.fracSplit(tail)[0] // i
+            # Wind left until high wouldn't imply carrying more, and
+            # carry is no bigger than i:
+            while i >= 2 and (high > (self.carry +1) * i -1 or abs(self.carry) > i):
                 high /= i
                 tail /= i
                 i -= 1
-                self.carry = _fracSplit(tail)[0] // i
+                high += s[i]
+                tail += s[i]
+                self.carry = self.fracSplit(tail)[0] // i
+            if self.carry < 0 and not any(s[j] for j in range(i)):
+                # Avoid generating a -ve digit:
+                self.carry = 0
             carried = self.carry * i
             self.__tail, self.__high = tail - carried, high - carried
             self.index = i
 
         def step(self, s):
-            tail, high = self.__tail * self.base, self.__high * self.base
+            self.__tail *= self.base
+            self.__high *= self.base
             self.__trim(s)
 
     def __init__(self, size, x = 1, base = 10):
+        """Set up to generate digits of exp(x).
+
+        Required parameter, size, is the natural n for which this is
+        to compute sum(: x**i/i! &larr;i |n).  Optional parameter x
+        (default 1) is the input to exp, optional parameter base
+        (default ten) is the number base with respect to which to
+        generate digits.  Both size and base must be positive integers
+        greater than 1.  Use Digex.enough(d, x, base) to construct an
+        instance that should produce at least d digits.\n"""
+        if not isinstance(size, (int, long)):
+            raise ValueError("Size must be integral", size)
         if not size > 1:
             raise ValueError("Way too small", size)
+        if not isinstance(base, (int, long)):
+            raise ValueError("Base must be integral", base)
+        if not base > 1:
+            raise ValueError("Incoherent base", base)
+
         self.__s = [0] * size
         self.__step = Digex.__Filler(self.__s, x, base)
         self.__gather()
@@ -197,6 +238,7 @@ class Digex (object):
         while i > 2:
             i -= 1
             carry, s[i] = divmod(s[i] +carry, i)
+            assert i > s[i] >= 0
         s[1] += carry
 
     def __iter__(self): return self
@@ -237,7 +279,7 @@ class Digex (object):
         goal = digits * ln(base) +offset
         func = lambda n, u=float(x): n * ln(n / u) +ln(n)/2 -n +ln(1 -u/n) -1/12./n
         fdash = lambda n, u=float(x): ln(n / u) +(0.5 +1/(n/u -1) +.25/3/n) / n
-        n = max((goal, 1, x))
+        ans = max((goal, 1, x))
 
         # Newton-Raphson, with a tweaked termination condition:
         err, fix = -1, 0
